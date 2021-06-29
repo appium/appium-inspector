87dc58dd6b10e7b656dfe9b113f5333b54852e2c
youngfreeFJS <471011042@qq.com>
2021-06-29 20:13:48 +0800

    remove: fixed the typo for reference errors

--- app/renderer/components/Session/Session.js
+++ app/renderer/components/Session/Session.js
@@ -62,7 +62,7 @@ export default class Session extends Component {
     return [
       <Spin spinning={!!sessionLoading} key="main">
         <div className={SessionStyles.sessionContainer}>
-          <div id='serverTypeTabs' className={SessionStyles.serverTabs}>
+          <div id='serverTypeTabs' className={SessionStyles.serverTab}>
             <Tabs activeKey={serverType} onChange={(tab) => this.handleSelectServerTab(tab)} className={SessionStyles.serverTabs}>
               {[
                 <TabPane tab={t('Appium Server')} key="remote">
