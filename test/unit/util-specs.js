5aeb93513e50c466d0f3437cfec5ea860028b9a0
Jonathan Lipps <jlipps@gmail.com>
2021-07-12 12:37:20 -0700

    fix: fix typo so that adding vendor prefixes works correctly (fix #68)

--- test/unit/util-specs.js
+++ test/unit/util-specs.js
@@ -1,6 +1,6 @@
 import chai from 'chai';
 import chaiAsPromised from 'chai-as-promised';
-import { getOptimalXPath, xmlToJSON } from '../../app/renderer/util';
+import { getOptimalXPath, xmlToJSON, addVendorPrefixes } from '../../app/renderer/util';
 import { DOMParser } from 'xmldom';
 import xpath from 'xpath';
 import sinon from 'sinon';
@@ -628,6 +628,22 @@ describe('util.js', function () {
         node.getAttribute = () => { throw new Error('Some unexpected error'); };
         should.not.exist(getOptimalXPath(doc, node));
       });
+    });
+  });
+
+  describe('#addVendorPrefixes', function () {
+    it('should convert unprefixed non-standard caps to use appium prefix', function () {
+      const caps = [{name: 'udid'}, {name: 'deviceName'}];
+      addVendorPrefixes(caps).should.eql([
+        {name: 'appium:udid'}, {name: 'appium:deviceName'}
+      ]);
+    });
+
+    it('should not convert already-prefixed or standard caps', function () {
+      const caps = [{name: 'udid'}, {name: 'browserName'}, {name: 'goog:chromeOptions'}];
+      addVendorPrefixes(caps).should.eql([
+        {name: 'appium:udid'}, {name: 'browserName'}, {name: 'goog:chromeOptions'}
+      ]);
     });
   });
 });
