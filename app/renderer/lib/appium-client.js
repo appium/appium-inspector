13b9aa6529e29b9668fb85b2e986a106ec70959b
Wim Selles <wim@wswebcreation.nl>
2021-09-28 17:38:55 +0200

    Fix: android hybrid app and chrome support
    * feat: fix android hybrid support
    
    - changed order of getting the source based on Webview manipulations
    - fix getting window size which will be different on Android Chrome/Webview
    - parse Android webview data to a similar object as for iOS
    
    * fix: read Android html
    
    - fixes after review comments
    - fix executeScript commands, they need to have a string instead of a function
    - fix parsing of html to XML, it gave all kinds of parsing errors
    
    * fix: add Chrome webview support for multiple tabs
    
    * test: add parseAndroidContexts test
    
    added integration tests to the tests
    
    * fix: fix html parsing
    
    - add unit tests
    
    * fix: fix after feedback

--- app/renderer/lib/appium-client.js
+++ app/renderer/lib/appium-client.js
@@ -1,7 +1,7 @@
 import _ from 'lodash';
 import Bluebird from 'bluebird';
-import { getWebviewStatusAddressBarHeight, parseSource, setHtmlElementAttributes } from './webview-helpers';
-import { SCREENSHOT_INTERACTION_MODE, APP_MODE } from '../components/Inspector/shared';
+import {getWebviewStatusAddressBarHeight, parseSource, setHtmlElementAttributes} from './webview-helpers';
+import {SCREENSHOT_INTERACTION_MODE, APP_MODE} from '../components/Inspector/shared';
 
 const NATIVE_APP = 'NATIVE_APP';
 let _instance = null;
@@ -23,13 +23,14 @@ export default class AppiumClient {
       elementId, // Optional. Element being operated on
       args = [], // Optional. Arguments passed to method
       skipRefresh = false, // Optional. Do we want the updated source and screenshot?
-      appMode = APP_MODE.NATIVE_APP, // Optional. Whether we're in a native or hybrid mode
+      appMode = APP_MODE.NATIVE, // Optional. Whether we're in a native or hybrid mode
     } = params;
 
     if (methodName === 'quit') {
       try {
         await this.driver.quit();
-      } catch (ign) {}
+      } catch (ign) {
+      }
 
       _instance = null;
 
@@ -123,14 +124,13 @@ export default class AppiumClient {
 
     let contextUpdate = {}, sourceUpdate = {}, screenshotUpdate = {}, windowSizeUpdate = {};
     if (!skipRefresh) {
-      sourceUpdate = await this.getSourceUpdate();
       screenshotUpdate = await this.getScreenshotUpdate();
       windowSizeUpdate = await this.getWindowUpdate();
-
       // only do context updates if user has selected web/hybrid mode (takes forever)
       if (appMode === APP_MODE.WEB_HYBRID) {
         contextUpdate = await this.getContextUpdate();
       }
+      sourceUpdate = await this.getSourceUpdate();
     }
     return {
       ...cachedEl,
@@ -202,8 +202,17 @@ export default class AppiumClient {
 
   async getWindowUpdate () {
     let windowSize, windowSizeError;
+    const {client: {capabilities: {deviceScreenSize, platformName}}} = this.driver;
     try {
-      windowSize = await this.driver.getWindowRect();
+      // The call doesn't need to be made for Android for two reasons
+      // - when appMode is hybrid Chrome driver doesn't know this command
+      // - the data is already on the driver
+      if (_.toLower(platformName) === 'android') {
+        const [width, height] = deviceScreenSize.split('x');
+        windowSize = {width, height, x: 0, y: 0};
+      } else {
+        windowSize = await this.driver.getWindowRect();
+      }
     } catch (e) {
       windowSizeError = e;
     }
@@ -212,8 +221,15 @@ export default class AppiumClient {
   }
 
   async getContextUpdate () {
-    let contexts, contextsError, currentContext, currentContextError, platformName, statBarHeight;
-
+    let contexts,
+        contextsError,
+        currentContext,
+        currentContextError,
+        pixelRatio,
+        platformName,
+        statBarHeight,
+        viewportRect,
+        webViewPosition;
     if (!await this.hasContextsCommand()) {
       return {currentContext: null, contexts: []};
     }
@@ -229,15 +245,39 @@ export default class AppiumClient {
       await this.driver.switchContext(NATIVE_APP);
     }
 
-    ({platformName, statBarHeight} = await this.driver.getSession());
+    ({platformName, pixelRatio, statBarHeight, viewportRect} = await this.driver.getSession());
+    const isAndroid = _.toLower(platformName) === 'android';
 
     try {
       contexts = await this.driver.executeScript('mobile:getContexts', []);
+      contexts = isAndroid ? this.parseAndroidContexts(contexts) : contexts;
     } catch (e) {
       contextsError = e;
     }
 
+
     if (currentContext !== NATIVE_APP) {
+      try {
+        // Get the webview offset
+        if (viewportRect) {
+          // The viewport rectangles are based on the screen density,
+          // iOS needs CSS pixels
+          webViewPosition = {
+            x: isAndroid ? viewportRect.left : Math.round(viewportRect.left / pixelRatio),
+            y: isAndroid ? viewportRect.top : Math.round(viewportRect.top / pixelRatio),
+          };
+        } else {
+          // Fallback
+          const el = await this.driver.findElement(
+            isAndroid ? 'xpath' : '-ios class chain',
+            isAndroid ? '//android.webkit.WebView' : '**/XCUIElementTypeWebView'
+          );
+          if (el) {
+            webViewPosition = await el.getRect();
+          }
+        }
+      } catch (ign) {
+      }
       await this.driver.switchContext(currentContext);
     }
 
@@ -247,14 +287,24 @@ export default class AppiumClient {
      */
     try {
       if (currentContext !== NATIVE_APP) {
-        const webviewStatusAddressBarHeight = await this.driver.executeScript(getWebviewStatusAddressBarHeight,
-          [{platformName, statBarHeight}]);
-        await this.driver.executeScript(setHtmlElementAttributes, [{
-          platformName,
-          webviewStatusAddressBarHeight
-        }]);
+        // Fallback if the webview position can't be determined,
+        // then do it based on the web context
+        if (!webViewPosition) {
+          webViewPosition = {
+            x: 0,
+            y: await this.driver.executeScript(
+              `return (${getWebviewStatusAddressBarHeight}).apply(null, arguments)`,
+              [{platformName, statBarHeight}],
+            ),
+          };
+        }
+        await this.driver.executeScript(
+          `return (${setHtmlElementAttributes}).apply(null, arguments)`,
+          [{platformName, webviewStatusAddressBarHeight: webViewPosition.y}],
+        );
       }
-    } catch (ign) {}
+    } catch (ign) {
+    }
 
     return {contexts, contextsError, currentContext, currentContextError};
   }
@@ -287,10 +337,79 @@ export default class AppiumClient {
     try {
       await this.driver.getContexts();
       return true;
-    } catch (ign) { }
+    } catch (ign) {
+    }
 
     // If the app under test returns non JSON format response
     return false;
+  }
+
+  /**
+   * Parse the Android contexts webview data
+   *
+   * Returns
+   * {
+   *   id: string;             // NATIVE_APP or the webview name
+   *   title?: string;         // the name of the page
+   *   url?: string;           // the url
+   *   bundleId?: string;      // for iOS
+   *   packageName?: string;   // for Android
+   *   handle?: string;        // the id of the active page in the webview of Android
+   * }[];
+   */
+  parseAndroidContexts (contexts) {
+    const parsedWebviews = [];
+
+    // Walk over every context and add all webviews into the parsedWebviews array
+    contexts
+      // Filter out all contexts that have a webviewName
+      .filter((webview) => _.has(webview, 'webviewName'))
+      // Now construct a new array with data
+      .map(({info, pages, webviewName}) => {
+        // The context result can have:
+        // - no pages => this might be Chrome running in the background
+        // - pages => this can be:
+        //   - Chrome with one or multiple tabs open
+        //   - A webview with one or multiple webviews
+        if (!pages) {
+          return;
+        }
+
+        pages.filter((page) => {
+          // The description is a string and:
+          // 1. can contain a JSON string for webviews which can contain
+          //    an `attached`-value telling if the webview is active
+          // 2. can be an empty string, this is most of the times for tabs
+          //    in Chrome
+          const description = _.has(page, 'description') ? page.description : '';
+          let descriptionJSON = {attached: false};
+          try {
+            descriptionJSON = JSON.parse(page.description);
+          } catch (ign) {}
+
+          // You can have multiple `type` of pages, like service workers
+          // We need to have pages with or 1. an attached view or 2. with an empty description
+          return page.type === 'page' && (description === '' || descriptionJSON.attached);
+        })
+          .map((page) => {
+            parsedWebviews.push({
+              id: webviewName,
+              ...(page && _.has(page, 'title') ? {title: page.title} : {}),
+              ...(page && _.has(page, 'url') ? {url: page.url} : {}),
+              ...(page && _.has(info, 'Android-Package') ? {packageName: info['Android-Package']} : {}),
+              ...(page && _.has(page, 'id') ? {handle: page.id} : {}),
+            });
+          });
+
+        return parsedWebviews;
+      });
+
+    return [
+      // The Android contexts dont have the `NATIVE_APP` context so add it here
+      {id: 'NATIVE_APP'},
+      // Add the parsedWebviews, but make sure to filter out all undefined webviews
+      ...parsedWebviews.filter(Boolean),
+    ];
   }
 }
 
---
+++ test/unit/webview-helpers-specs.js
@@ -0,0 +1,26 @@
+import {promises as fs} from 'fs';
+import {join} from 'path';
+import chai from 'chai';
+import chaiAsPromised from 'chai-as-promised';
+import {parseSource} from '../../app/renderer/lib/webview-helpers';
+
+chai.use(chaiAsPromised);
+
+describe('webview-helpers.js', function () {
+  describe('#parseSource', function () {
+    it('should parse html to proper xml', async function () {
+      const output = parseSource(
+        await fs.readFile(
+          join(__dirname, './mocks/appium.page.original.html'),
+          'utf8',
+        ));
+
+      output.should.eql(
+        await fs.readFile(
+          join(__dirname, './mocks/appium.page.parsed.html'),
+          'utf8',
+        )
+      );
+    });
+  });
+});
