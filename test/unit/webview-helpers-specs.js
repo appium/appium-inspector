8fd478e20e83125de6e3ca925daef2ec9d2bb5e1
Wim Selles <wim@wswebcreation.nl>
2021-09-28 13:28:27 +0200

    fix: fix after feedback

--- test/unit/webview-helpers-specs.js
+++ test/unit/webview-helpers-specs.js
@@ -1,4 +1,4 @@
-import {readFileSync} from 'fs';
+import {promises as fs} from 'fs';
 import {join} from 'path';
 import chai from 'chai';
 import chaiAsPromised from 'chai-as-promised';
@@ -8,15 +8,15 @@ chai.use(chaiAsPromised);
 
 describe('webview-helpers.js', function () {
   describe('#parseSource', function () {
-    it('should parse html to proper xml', function () {
+    it('should parse html to proper xml', async function () {
       const output = parseSource(
-        readFileSync(
+        await fs.readFile(
           join(__dirname, './mocks/appium.page.original.html'),
           'utf8',
         ));
 
       output.should.eql(
-        readFileSync(
+        await fs.readFile(
           join(__dirname, './mocks/appium.page.parsed.html'),
           'utf8',
         )
