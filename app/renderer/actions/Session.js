d2c00bb59e727ab461d0c15a1edb8373474fadf3
lastverb <lastverb@gmail.com>
2021-12-07 14:36:07 +0100

    fix: Being blocked by webdriver client when using bitbar

--- app/renderer/actions/Session.js
+++ app/renderer/actions/Session.js
@@ -310,8 +310,9 @@ export function newSession (caps, attachSessId = null) {
           });
           return;
         }
-        desiredCapabilities.testdroid_source = 'appiumdesktop';
-        desiredCapabilities.testdroid_apiKey = accessKey;
+        desiredCapabilities['bitbar:options'] = {};
+        desiredCapabilities['bitbar:options'].source = 'appiumdesktop';
+        desiredCapabilities['bitbar:options'].apiKey = accessKey;
         https = session.server.bitbar.ssl = true;
         break;
       case ServerTypes.kobiton:
