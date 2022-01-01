8cbb1410b9caea1f70ac60e9437e0a4554b387ba
Kazuaki Matsuo <fly.49.89.over@gmail.com>
2022-01-01 10:11:22 -0800

    feat: update guesture in python for w3c (#254)

--- app/renderer/lib/client-frameworks/python.js
+++ app/renderer/lib/client-frameworks/python.js
@@ -22,6 +22,12 @@ class PythonFramework extends Framework {
 from appium import webdriver
 from appium.webdriver.common.appiumby import AppiumBy
 
+# For W3C actions
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.common.actions import interaction
+from selenium.webdriver.common.actions.action_builder import ActionBuilder
+from selenium.webdriver.common.actions.pointer_input import PointerInput
+
 caps = {}
 ${capStr}
 
@@ -75,15 +81,24 @@ driver.quit()`;
   }
 
   codeFor_tap (varNameIgnore, varIndexIgnore, x, y) {
-    return `TouchAction(driver).tap(x=${x}, y=${y}).perform()`;
+    return `actions = ActionChains(driver)
+actions.w3c_actions = ActionBuilder(driver, mouse=PointerInput(interaction.POINTER_TOUCH, "touch"))
+actions.w3c_actions.pointer_action.move_to_location(${x}, ${y})
+actions.w3c_actions.pointer_action.pointer_down()
+actions.w3c_actions.pointer_action.pause(0.1)
+actions.w3c_actions.pointer_action.release()
+actions.perform()
+    `;
   }
 
   codeFor_swipe (varNameIgnore, varIndexIgnore, x1, y1, x2, y2) {
-    return `TouchAction(driver) \
-  .press(x=${x1}, y=${y1}) \
-  .move_to(x=${x2}, y=${y2}) \
-  .release() \
-  .perform()
+    return `actions = ActionChains(driver)
+actions.w3c_actions = ActionBuilder(driver, mouse=PointerInput(interaction.POINTER_TOUCH, "touch"))
+actions.w3c_actions.pointer_action.move_to_location(${x1}, ${y1})
+actions.w3c_actions.pointer_action.pointer_down()
+actions.w3c_actions.pointer_action.move_to_location(${x2}, ${y2})
+actions.w3c_actions.pointer_action.release()
+actions.perform()
     `;
   }
 
