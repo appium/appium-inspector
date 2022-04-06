6b4941c301226b5489598c58a0b648515c7c76c7
Wim Selles <wim@wswebcreation.nl>
2022-04-06 16:11:42 +0200

    fix: switch back to native context for native app mode

--- app/renderer/lib/appium-client.js
+++ app/renderer/lib/appium-client.js
@@ -3,7 +3,7 @@ import Bluebird from 'bluebird';
 import {getWebviewStatusAddressBarHeight, parseSource, setHtmlElementAttributes} from './webview-helpers';
 import {SCREENSHOT_INTERACTION_MODE, APP_MODE} from '../components/Inspector/shared';
 
-const NATIVE_APP = 'NATIVE_APP';
+export const NATIVE_APP = 'NATIVE_APP';
 let _instance = null;
 
 export default class AppiumClient {
