8dec5898857e8eaef9dc77cb8adbea3826d77fd5
dpgraham <dpgraham.85@gmail.com>
2022-05-02 00:45:12 -0700

    fix launcher

--- app/main/helpers.js
+++ app/main/helpers.js
@@ -16,12 +16,17 @@ export function setSavedEnv () {
 }
 
 export function getAppiumSessionFilePath (argv, isPackaged, isDev) {
+  if (isDev) {
+    // do not use file launcher in dev mode because argv is different
+    // then it is in production
+    return false;
+  }
   if (process.platform === 'darwin' && !isDev) {
     // packaged macOS apps do not receive args from process.argv, they
     // receive the filepath argument from the `electron.app.on('open-file', cb)` event
     return false;
   }
-  const argvIndex = isPackaged ? 1 : 2;
+  let argvIndex = isPackaged ? 1 : 2;
   return argv[argvIndex];
 }
 
