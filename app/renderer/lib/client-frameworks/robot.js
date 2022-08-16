e8b3176e7800978f4b2a56187e3fcb5fe874256f
Kazuaki Matsuo <fly.49.89.over@gmail.com>
2022-08-16 01:03:11 -0700

    fix lint

--- app/renderer/lib/client-frameworks/robot.js
+++ app/renderer/lib/client-frameworks/robot.js
@@ -64,9 +64,9 @@ ${this.indent(code, 4)}
     }
     //TODO: in the robot case, we need the ID on the codeFor_ for execution
     this.lastID = `${strategy}=${locator}`;
-    
-    if(this.lastID.includes("accessibility id")){
-      this.lastID = this.lastID.replace("accessibility id", 'accessibility_id');
+
+    if (this.lastID.includes('accessibility id')) {
+      this.lastID = this.lastID.replace('accessibility id', 'accessibility_id');
     }
 
     return `# ${this.lastID}`;
