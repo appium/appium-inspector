ab1701827f878377d10ffe297bafac9f301d58e6
Edgars Eglītis <edgars.eglitis@outlook.com>
2023-03-29 15:41:27 +0300

    style: reduce empty space in server tabs

--- app/renderer/components/Session/ServerTabCustom.js
+++ app/renderer/components/Session/ServerTabCustom.js
@@ -12,27 +12,25 @@ export default class ServerTabCustom extends Component {
 
     return <Form>
       <Row gutter={8}>
-        <Col span={12}>
+        <Col span={9}>
           <FormItem>
             <Input id='customServerHost' placeholder={DEFAULT_SERVER_HOST} addonBefore={t('Remote Host')} value={server.remote.hostname}
               onChange={(e) => setServerParam('hostname', e.target.value)} />
           </FormItem>
         </Col>
-        <Col span={12}>
+        <Col span={4}>
           <FormItem>
             <Input id='customServerPort' placeholder={DEFAULT_SERVER_PORT} addonBefore={t('Remote Port')} value={server.remote.port}
               onChange={(e) => setServerParam('port', e.target.value)} />
           </FormItem>
         </Col>
-      </Row>
-      <Row gutter={8}>
-        <Col span={12}>
+        <Col span={9}>
           <FormItem>
             <Input id='customServerPath' placeholder={DEFAULT_SERVER_PATH} addonBefore={t('Remote Path')} value={server.remote.path}
               onChange={(e) => setServerParam('path', e.target.value)} />
           </FormItem>
         </Col>
-        <Col span={3}>
+        <Col span={2}>
           <FormItem>
             <Checkbox id='customServerSSL' checked={!!server.remote.ssl} value={server.remote.ssl} onChange={(e) => setServerParam('ssl', e.target.checked)}>{t('SSL')}</Checkbox>
           </FormItem>
