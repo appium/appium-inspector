ccd396f50516708aaf1bb88d20762fa8d4f77f1c
Edgars Eglītis <edgars.eglitis@outlook.com>
2023-04-19 16:09:50 +0300

    chore: merge configs folders

--- app/configs/app.config.js
+++ app/configs/app.config.js
@@ -1,3 +1,26 @@
-import defaultConfig from '../../gui-common/configs/app.config.default';
+const config = {
+  platform: process.platform,
+  languages: ['en', 'de', 'ru', 'ja', 'zh-CN', 'ko', 'hi', 'kn', 'ml-IN', 'pa-IN', 'te', 'pt-BR', 'it'],
+  namespace: 'translation',
+};
 
-export default defaultConfig;
+function getI18NextOptions (settings, config, backend) {
+  return {
+    backend,
+    // debug: true,
+    // saveMissing: true,
+    interpolation: {
+      escapeValue: false
+    },
+    lng: settings && settings.getSync('PREFERRED_LANGUAGE', 'en') || 'en',
+    fallbackLng: config.fallbackLng,
+    whitelist: config.languages,
+    react: {
+      wait: false
+    }
+  };
+}
+
+
+export default config;
+export { getI18NextOptions };
