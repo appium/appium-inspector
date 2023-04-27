007a94e772ef86aee1a34db87246f8084d12de11
Edgars Eglītis <edgars.eglitis@outlook.com>
2023-04-27 14:38:47 +0300

    chore: simplify i18n config imports

--- app/configs/i18next.config.js
+++ app/configs/i18next.config.js
@@ -1,9 +1,8 @@
 import i18n from 'i18next';
+import { getI18NextOptions } from './app.config';
 import { i18NextBackend, i18NextBackendOptions } from '../renderer/polyfills';
-import config, { getI18NextOptions } from './app.config';
-import settings from '../shared/settings';
 
-const i18nextOptions = getI18NextOptions(settings, config, i18NextBackendOptions);
+const i18nextOptions = getI18NextOptions(i18NextBackendOptions);
 
 if (!i18n.isInitialized) {
   i18n
