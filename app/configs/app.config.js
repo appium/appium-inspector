a4ed28bc98018ca3f536b51a3292e8a43dd2ac8a
Edgars Eglītis <edgars.eglitis@outlook.com>
2023-05-02 15:09:26 +0300

    feat: add Ukrainian language

--- app/configs/app.config.js
+++ app/configs/app.config.js
@@ -1,6 +1,6 @@
 const config = {
   platform: process.platform,
-  languages: ['de', 'en', 'es-ES', 'fa', 'hi', 'it', 'ja', 'ko', 'kn', 'ml-IN', 'pa-IN', 'pl', 'pt-BR', 'pt-PT', 'ru', 'te', 'zh-CN'],
+  languages: ['de', 'en', 'es-ES', 'fa', 'hi', 'it', 'ja', 'ko', 'kn', 'ml-IN', 'pa-IN', 'pl', 'pt-BR', 'pt-PT', 'ru', 'te', 'uk', 'zh-CN'],
   fallbackLng: 'en',
   namespace: 'translation',
 };
