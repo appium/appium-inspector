67166b61f3c5edf453237372c129f8e068565fd0
Edgars Eglītis <edgars.eglitis@outlook.com>
2023-05-18 16:25:37 +0300

    chore: fix console error about missing keys

--- app/renderer/components/Inspector/LocatorTestModal.js
+++ app/renderer/components/Inspector/LocatorTestModal.js
@@ -47,18 +47,15 @@ class LocatorTestModal extends Component {
       confirmLoading={isSearchingForElements}
       onCancel={this.onCancel.bind(this)}
       footer=
-        {[
-          locatedElements &&
+        {<>
+          {locatedElements &&
           <Button onClick={(e) => e.preventDefault() || clearSearchResults()}>
             Back
-          </Button>,
+          </Button>}
           <Button onClick={this.onSubmit.bind(this)} type="primary">
-            {locatedElements ?
-              t('Done')
-              :
-              t('Search')}
+            {locatedElements ? t('Done') : t('Search')}
           </Button>
-        ]}>
+        </>}>
       {!locatedElements && <ElementLocator {...this.props} />}
       {locatedElements && <LocatedElements {...this.props} />}
     </Modal>;
