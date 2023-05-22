a5379bbf88d333c0c6149c55f80212be2f81afbd
Edgars Eglītis <edgars.eglitis@outlook.com>
2023-05-22 14:42:05 +0300

    fix: detect buttons based on automation name

--- app/renderer/components/Inspector/ElementLocator.js
+++ app/renderer/components/Inspector/ElementLocator.js
@@ -3,32 +3,33 @@ import { Input, Radio, Row } from 'antd';
 import InspectorStyles from './Inspector.css';
 import { withTranslation } from '../../util';
 
+const STRAT_ID = ['id', 'Id'];
+const STRAT_XPATH = ['xpath', 'XPath'];
+const STRAT_NAME = ['name', 'Name'];
+const STRAT_CLASS_NAME = ['class name', 'Class Name'];
+const STRAT_ACCESSIBILITY_ID = ['accessibility id', 'Accessibility ID'];
+const STRAT_PREDICATE = ['-ios predicate string', 'Predicate String'];
+const STRAT_CLASS_CHAIN = ['-ios class chain', 'Class Chain'];
+const STRAT_UIAUTOMATOR = ['-android uiautomator', 'UIAutomator'];
+const STRAT_DATAMATCHER = ['-android datamatcher', 'DataMatcher'];
+const STRAT_VIEWTAG = ['-android viewtag', 'View Tag'];
+
 const locatorStrategies = (driver) => {
-  let baseLocatorStrategies = [
-    ['id', 'Id'],
-    ['xpath', 'XPath'],
-    ['name', 'Name'],
-    ['class name', 'Class Name'],
-    ['accessibility id', 'Accessibility ID']
-  ];
-  if (['ios', 'mac'].includes(driver.client.capabilities.platformName.toLowerCase())) {
-    baseLocatorStrategies.push(
-      ['-ios predicate string', 'Predicate String'],
-      ['-ios class chain', 'Class Chain']
-    );
-  } else if (driver.client.isAndroid) {
-    if (driver.client.capabilities.automationName.toLowerCase() === 'espresso') {
-      baseLocatorStrategies.push(
-        ['-android datamatcher', 'DataMatcher'],
-        ['-android viewtag', 'View Tag']
-      );
-    } else {
-      baseLocatorStrategies.push(
-        ['-android uiautomator', 'UIAutomator'],
-      );
-    }
+  const automationName = driver.client.capabilities.automationName.toLowerCase();
+  let strategies = [STRAT_ID, STRAT_XPATH, STRAT_NAME, STRAT_CLASS_NAME, STRAT_ACCESSIBILITY_ID];
+  switch (automationName) {
+    case 'xcuitest':
+    case 'mac2':
+      strategies.push(STRAT_PREDICATE, STRAT_CLASS_CHAIN);
+      break;
+    case 'espresso':
+      strategies.push(STRAT_DATAMATCHER, STRAT_VIEWTAG);
+      break;
+    case 'uiautomator2':
+      strategies.push(STRAT_UIAUTOMATOR);
+      break;
   }
-  return baseLocatorStrategies;
+  return strategies;
 };
 
 class ElementLocator extends Component {
