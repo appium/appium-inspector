3a81b951b660d1e40b9d84662837495126a35648
Edgars Eglītis <edgars.eglitis@outlook.com>
2023-06-01 13:08:48 +0300

    chore: use Redux DevTools in dev only

--- app/renderer/store/configureStore.development.js
+++ app/renderer/store/configureStore.development.js
@@ -11,32 +11,33 @@ const rootReducer = createRootReducer(history);
 
 const configureStore = (initialState) => {
   const middleware = [];
+  let composeEnhancers = compose;
 
   // Thunk Middleware
   middleware.push(thunk);
 
-  // Logging Middleware - dev only
+  // Additional development tools
   if (process.env.NODE_ENV === 'development') {
+    // Logging Middleware
     const { createLogger } = require('redux-logger');
     const logger = createLogger({
       level: 'info',
       collapsed: true
     });
-
     middleware.push(logger);
+
+    // Use Redux DevTools Extension if installed
+    if (window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) {
+      composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
+        // Options: https://github.com/zalmoxisus/redux-devtools-extension/blob/master/docs/API/Arguments.md
+        actionCreators: {...actions, push}
+      });
+    }
   }
 
   // Router Middleware
   const router = routerMiddleware(history);
   middleware.push(router);
-
-  // If Redux DevTools Extension is installed use it, otherwise use Redux compose
-  const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
-    ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
-      // Options: https://github.com/zalmoxisus/redux-devtools-extension/blob/master/docs/API/Arguments.md
-      actionCreators: {...actions, push}
-    })
-    : compose;
 
   // Apply Middleware & Compose Enhancers
   const enhancer = composeEnhancers(
