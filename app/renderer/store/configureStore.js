f638b3f405901fa91d98ef34fd4b5e4ebed34219
Edgars Eglītis <edgars.eglitis@outlook.com>
2023-06-01 12:22:22 +0300

    chore: remove duplicate export

--- app/renderer/store/configureStore.js
+++ app/renderer/store/configureStore.js
@@ -1,15 +1,3 @@
-import configureStoreDev from './configureStore.development';
-import configureStoreProd from './configureStore.production';
-
-const selectedConfigureStore =
-  process.env.NODE_ENV === 'production'
-    ? configureStoreProd
-    : configureStoreDev;
-
-export const { configureStore } = selectedConfigureStore;
-
-export const { history } = selectedConfigureStore;
-
 if (process.env.NODE_ENV === 'production') {
   module.exports = require('./configureStore.production');
 } else {
