9b211e02ae37e8afd31929f0c23c61d135256786
Edgars Eglītis <edgars.eglitis@outlook.com>
2023-06-01 10:11:27 +0300

    chore: remove unused/deprecated eslint flags

--- app/renderer/store/configureStore.development.js
+++ app/renderer/store/configureStore.development.js
@@ -39,14 +39,12 @@ const configureStore = (initialState) => {
     ...routerActions
   };
   // If Redux DevTools Extension is installed use it, otherwise use Redux compose
-  /* eslint-disable no-underscore-dangle */
   const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
     ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
       // Options: http://extension.remotedev.io/docs/API/Arguments.html
       actionCreators
     })
     : compose;
-  /* eslint-enable no-underscore-dangle */
 
   // Apply Middleware & Compose Enhancers
   enhancers.push(applyMiddleware(...middleware));
@@ -58,7 +56,6 @@ const configureStore = (initialState) => {
   if (module.hot) {
     module.hot.accept(
       '../reducers',
-      // eslint-disable-next-line global-require
       () => store.replaceReducer(require('../reducers').default)
     );
   }
