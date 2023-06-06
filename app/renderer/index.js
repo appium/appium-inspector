3e8a8c3ba05ba9bc65b84925505088c5fa01c1b4
Edgars Eglītis <edgars.eglitis@outlook.com>
2023-06-06 15:55:29 +0300

    chore: switch to React 18 root API

--- app/renderer/index.js
+++ app/renderer/index.js
@@ -1,26 +1,26 @@
 import React from 'react';
-import { render } from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import Root from './containers/Root';
 import ErrorBoundary from './components/ErrorBoundary/ErrorBoundary';
 import configureStore from './store';
 
 const { store, history } = configureStore();
+const container = document.getElementById('root');
+const root = createRoot(container);
 
-render(
+root.render(
   <ErrorBoundary>
     <Root store={store} history={history} />
-  </ErrorBoundary>,
-  document.getElementById('root')
+  </ErrorBoundary>
 );
 
 if (module.hot) {
   module.hot.accept('./containers/Root', () => {
     const NextRoot = require('./containers/Root').default;
-    render(
+    root.render(
       <AppContainer>
         <NextRoot store={store} history={history} />
-      </AppContainer>,
-      document.getElementById('root')
+      </AppContainer>
     );
   });
 }
