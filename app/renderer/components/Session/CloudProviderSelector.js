f5253ec89c1e23652781de300b33a9c9fbe9ef9a
Edgars Eglītis <edgars.eglitis@outlook.com>
2023-06-16 12:56:44 +0300

    chore(react): migrate CloudProviderSelector

--- app/renderer/components/Session/CloudProviderSelector.js
+++ app/renderer/components/Session/CloudProviderSelector.js
@@ -1,49 +1,50 @@
-import React, { Component } from 'react';
+import React from 'react';
 import _ from 'lodash';
 import { Modal, Row, Col, Button } from 'antd';
 import CloudProviders from './CloudProviders';
 import SessionStyles from './Session.css';
 import { BUTTON } from '../AntdTypes';
 
-export default class CloudProviderSelector extends Component {
+const CloudProviderSelector = (props) => {
 
-  toggleVisibleProvider (providerName) {
-    const {addVisibleProvider, removeVisibleProvider, visibleProviders = []} = this.props;
+  const { visibleProviders = [], addVisibleProvider, removeVisibleProvider,
+          isAddingCloudProvider, stopAddCloudProvider, t } = props;
+
+  const footer = <Button key="back" type={BUTTON.PRIMARY} onClick={stopAddCloudProvider}>{t('Done')}</Button>;
+  const providersGrid = _.chunk(_.keys(CloudProviders), 2); // Converts list of providers into list of pairs of providers
+
+  const toggleVisibleProvider = (providerName) => {
     if (visibleProviders.includes(providerName)) {
       removeVisibleProvider(providerName);
     } else {
       addVisibleProvider(providerName);
     }
-  }
+  };
 
-  render () {
-    const {t, isAddingCloudProvider, stopAddCloudProvider, visibleProviders = []} = this.props;
-    const providersGrid = _.chunk(_.keys(CloudProviders), 2); // Converts list of providers into list of pairs of providers
-    const footer = [<Button key="back" type={BUTTON.PRIMARY} onClick={stopAddCloudProvider}>{t('Done')}</Button>];
+  return <Modal key="modal"
+    className={SessionStyles.cloudProviderModal}
+    open={isAddingCloudProvider}
+    onCancel={stopAddCloudProvider}
+    footer={footer}
+    title={t('Select Cloud Providers')}>
+    {[
+      ..._.map(providersGrid, (row, key) => <Row gutter={16} key={key}>{
+        [
+          ..._(row).map((providerName) => {
+            const providerIsVisible = visibleProviders.includes(providerName);
+            const style = {};
+            if (providerIsVisible) {
+              style.borderColor = '#40a9ff';
+            }
+            const provider = CloudProviders[providerName];
+            return provider && <Col span={12} key={providerName}>
+              <Button role="checkbox" style={style} onClick={() => toggleVisibleProvider(providerName)}><img src={provider.logo} /></Button>
+            </Col>;
+          })
+        ]
+      }</Row>)
+    ]}
+  </Modal>;
+};
 
-    return <Modal key="modal"
-      className={SessionStyles.cloudProviderModal}
-      open={isAddingCloudProvider}
-      onCancel={stopAddCloudProvider}
-      footer={footer}
-      title={t('Select Cloud Providers')}>
-      {[
-        ..._.map(providersGrid, (row, key) => <Row gutter={16} key={key}>{
-          [
-            ..._(row).map((providerName) => {
-              const providerIsVisible = visibleProviders.includes(providerName);
-              const style = {};
-              if (providerIsVisible) {
-                style.borderColor = '#40a9ff';
-              }
-              const provider = CloudProviders[providerName];
-              return provider && <Col span={12} key={providerName}>
-                <Button role="checkbox" style={style} onClick={() => this.toggleVisibleProvider(providerName)}><img src={provider.logo} /></Button>
-              </Col>;
-            })
-          ]
-        }</Row>)
-      ]}
-    </Modal>;
-  }
-}
+export default CloudProviderSelector;
