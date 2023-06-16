1ecaefdfc4e6e657591881c58c925217ffc531a4
Edgars Eglītis <edgars.eglitis@outlook.com>
2023-06-16 12:28:22 +0300

    chore(react): migrate FormattedCaps

--- app/renderer/components/Session/FormattedCaps.js
+++ app/renderer/components/Session/FormattedCaps.js
@@ -1,19 +1,19 @@
-import React, { Component } from 'react';
+import React from 'react';
 import formatJSON from 'format-json';
 import SessionStyles from './Session.css';
 import { Card, Button, Alert, Tooltip } from 'antd';
 import { getCapsObject } from '../../actions/Session.js';
-import {
-  CloseOutlined,
-  SaveOutlined,
-  EditOutlined
-} from '@ant-design/icons';
+import { CloseOutlined, SaveOutlined, EditOutlined } from '@ant-design/icons';
 import { ALERT } from '../AntdTypes';
 
-export default class FormattedCaps extends Component {
+export const FormattedCaps = (props) => {
 
-  setCapsTitle (title, isEditingDesiredCapsName) {
-    const {setDesiredCapsName, desiredCapsName} = this.props;
+  const {caps, title, desiredCapsName, setDesiredCapsName, isEditingDesiredCapsName,
+         startDesiredCapsNameEditor, abortDesiredCapsNameEditor, saveDesiredCapsName,
+         isEditingDesiredCaps, startDesiredCapsEditor, abortDesiredCapsEditor,
+         saveRawDesiredCaps, setRawDesiredCaps, rawDesiredCaps, isValidCapsJson, invalidCapsJsonReason, t} = props;
+
+  const setCapsTitle = () => {
     if (!title) {
       return 'JSON Representation';
     } else if (!isEditingDesiredCapsName) {
@@ -25,10 +25,9 @@ export default class FormattedCaps extends Component {
         className={SessionStyles.capsEditorTitle}
       />;
     }
-  }
+  };
 
-  setCapsTitleButtons (title, isEditingDesiredCapsName, t) {
-    const {startDesiredCapsNameEditor, abortDesiredCapsNameEditor, saveDesiredCapsName} = this.props;
+  const setCapsTitleButtons = () => {
     if (!title) {
       return null;
     } else if (!isEditingDesiredCapsName) {
@@ -55,48 +54,42 @@ export default class FormattedCaps extends Component {
           className={SessionStyles.capsNameEditorButton} />
       </Tooltip></div>;
     }
-  }
+  };
 
-  getFormattedJSON (caps) {
-    return formatJSON.plain(getCapsObject(caps));
-  }
+  return caps && <Card
+    className={SessionStyles.formattedCaps}
+    title={setCapsTitle()}
+    extra={setCapsTitleButtons()}
+  >
+    <div className={SessionStyles.capsEditorControls}>
+      {isEditingDesiredCaps && <Tooltip title={t('Cancel')}>
+        <Button
+          onClick={abortDesiredCapsEditor}
+          icon={<CloseOutlined/>}
+          className={SessionStyles.capsEditorButton} />
+      </Tooltip> }
+      {isEditingDesiredCaps && <Tooltip title={t('Save')}>
+        <Button
+          onClick={saveRawDesiredCaps}
+          icon={<SaveOutlined/>}
+          className={SessionStyles.capsEditorButton} />
+      </Tooltip>}
+      {!isEditingDesiredCaps && <Tooltip title={t('Edit Raw JSON')} placement="topRight" >
+        <Button
+          onClick={startDesiredCapsEditor}
+          icon={<EditOutlined/>} />
+      </Tooltip> }
+    </div>
+    {isEditingDesiredCaps && <div className={SessionStyles.capsEditor}>
+      <textarea onChange={(e) => setRawDesiredCaps(e.target.value)} value={rawDesiredCaps}
+        className={`${SessionStyles.capsEditorBody} ${isValidCapsJson ? SessionStyles.capsEditorBodyFull : SessionStyles.capsEditorBodyResized}`}
+      />
+      {!isValidCapsJson && <Alert message={invalidCapsJsonReason} type={ALERT.ERROR} />}
+    </div>}
+    {!isEditingDesiredCaps && <div className={SessionStyles.formattedCapsBody}>
+      <pre>{formatJSON.plain(getCapsObject(caps))}</pre>
+    </div>}
+  </Card>;
+};
 
-  render () {
-    const {caps, title, isEditingDesiredCapsName, isEditingDesiredCaps, startDesiredCapsEditor, abortDesiredCapsEditor,
-           saveRawDesiredCaps, setRawDesiredCaps, rawDesiredCaps, isValidCapsJson, invalidCapsJsonReason, t} = this.props;
-    return caps && <Card
-      className={SessionStyles.formattedCaps}
-      title={this.setCapsTitle(title, isEditingDesiredCapsName)}
-      extra={this.setCapsTitleButtons(title, isEditingDesiredCapsName, t)}
-    >
-      <div className={SessionStyles.capsEditorControls}>
-        {isEditingDesiredCaps && <Tooltip title={t('Cancel')}>
-          <Button
-            onClick={abortDesiredCapsEditor}
-            icon={<CloseOutlined/>}
-            className={SessionStyles.capsEditorButton} />
-        </Tooltip> }
-        {isEditingDesiredCaps && <Tooltip title={t('Save')}>
-          <Button
-            onClick={saveRawDesiredCaps}
-            icon={<SaveOutlined/>}
-            className={SessionStyles.capsEditorButton} />
-        </Tooltip>}
-        {!isEditingDesiredCaps && <Tooltip title={t('Edit Raw JSON')} placement="topRight" >
-          <Button
-            onClick={startDesiredCapsEditor}
-            icon={<EditOutlined/>} />
-        </Tooltip> }
-      </div>
-      {isEditingDesiredCaps && <div className={SessionStyles.capsEditor}>
-        <textarea onChange={(e) => setRawDesiredCaps(e.target.value)} value={rawDesiredCaps}
-          className={`${SessionStyles.capsEditorBody} ${isValidCapsJson ? SessionStyles.capsEditorBodyFull : SessionStyles.capsEditorBodyResized}`}
-        />
-        {!isValidCapsJson && <Alert message={invalidCapsJsonReason} type={ALERT.ERROR} />}
-      </div>}
-      {!isEditingDesiredCaps && <div className={SessionStyles.formattedCapsBody}>
-        <pre>{this.getFormattedJSON(caps)}</pre>
-      </div>}
-    </Card>;
-  }
-}
+export default FormattedCaps;
