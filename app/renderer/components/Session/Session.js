78e3b2bfc807a557448079f20c420d2e2f34cc41
Edgars Eglītis <edgars.eglitis@outlook.com>
2023-06-16 16:19:38 +0300

    chore(react): migrate Session

--- app/renderer/components/Session/Session.js
+++ app/renderer/components/Session/Session.js
@@ -1,5 +1,5 @@
 import { shell, ipcRenderer } from '../../polyfills';
-import React, { Component } from 'react';
+import React, { useEffect } from 'react';
 import _ from 'lodash';
 import CapabilityEditor from './CapabilityEditor';
 import SavedSessions from './SavedSessions';
@@ -14,12 +14,27 @@ import { LinkOutlined } from '@ant-design/icons';
 import { BUTTON } from '../AntdTypes';
 
 const ADD_CLOUD_PROVIDER = 'addCloudProvider';
+const CAPS_DOCS_LINK = 'https://appium.io/docs/en/latest/guides/caps/';
 
-export default class Session extends Component {
+const Session = (props) => {
+  const { tabKey, switchTabs, serverType, server, visibleProviders = [],
+          caps, capsUUID, capsName, isCapsDirty, isEditingDesiredCaps, requestSaveAsModal,
+          saveSession, newSession, savedSessions, newSessionLoading, attachSessId, t } = props;
 
-  componentDidMount () {
-    const {setLocalServerParams, getSavedSessions, setSavedServerParams, setStateFromAppiumFile,
-           setVisibleProviders, getRunningSessions, bindWindowClose, initFromQueryString, saveFile, switchTabs} = this.props;
+  const isAttaching = tabKey === 'attach';
+
+  const handleSelectServerTab = async (tab) => {
+    const { changeServerType, addCloudProvider } = props;
+    if (tab === ADD_CLOUD_PROVIDER) {
+      addCloudProvider();
+      return;
+    }
+    await changeServerType(tab);
+  };
+
+  useEffect(() => {
+    const { setLocalServerParams, getSavedSessions, setSavedServerParams, setStateFromAppiumFile,
+            setVisibleProviders, getRunningSessions, bindWindowClose, initFromQueryString, saveFile } = props;
     (async () => {
       try {
         bindWindowClose();
@@ -31,94 +46,67 @@ export default class Session extends Component {
         getRunningSessions();
         await initFromQueryString();
         await setStateFromAppiumFile();
-        ipcRenderer.on('open-file', (evt, filePath) => {
-          setStateFromAppiumFile(filePath);
-        });
-        ipcRenderer.on('save-file', (evt, filePath) => {
-          saveFile(filePath);
-        });
+        ipcRenderer.on('open-file', (_, filePath) => setStateFromAppiumFile(filePath));
+        ipcRenderer.on('save-file', (_, filePath) => saveFile(filePath));
       } catch (e) {
         console.error(e); // eslint-disable-line no-console
       }
     })();
-  }
+  }, []);
 
-  async handleSelectServerTab (tab) {
-    const {changeServerType, addCloudProvider} = this.props;
-    if (tab === ADD_CLOUD_PROVIDER) {
-      addCloudProvider();
-      return;
-    }
-    await changeServerType(tab);
-  }
-
-  render () {
-    const {savedSessions, tabKey, switchTabs,
-           serverType, server,
-           requestSaveAsModal, newSession, caps, capsUUID, capsName, saveSession,
-           visibleProviders = [],
-           isCapsDirty, isEditingDesiredCaps, newSessionLoading, attachSessId, t} = this.props;
-
-    const isAttaching = tabKey === 'attach';
-
-    return [
-      <Spin spinning={!!newSessionLoading} key="main">
-        <div className={SessionStyles.sessionContainer}>
-          <div id='serverTypeTabs' className={SessionStyles.serverTab}>
-            <Tabs activeKey={serverType} onChange={(tab) => this.handleSelectServerTab(tab)} className={SessionStyles.serverTabs} items={[{
-              label: t('Appium Server'), key: 'remote', children:
-                <ServerTabCustom {...this.props} />
-            },
+  return [
+    <Spin spinning={!!newSessionLoading} key="main">
+      <div className={SessionStyles.sessionContainer}>
+        <div id='serverTypeTabs' className={SessionStyles.serverTab}>
+          <Tabs activeKey={serverType} onChange={(tab) => handleSelectServerTab(tab)} className={SessionStyles.serverTabs} items={[
+            {label: t('Appium Server'), key: 'remote', children: <ServerTabCustom {...props} />},
             ..._(visibleProviders).map((providerName) => {
               const provider = CloudProviders[providerName];
               if (!provider) {
                 return true;
               }
-
-              return {label: <div>{provider.tabhead()}</div>, key: providerName, children: provider.tab(this.props)};
+              return {label: <div>{provider.tabhead()}</div>, key: providerName, children: provider.tab(props)};
             }),
             {label: <span className='addCloudProviderTab'>{ t('Select Cloud Providers') }</span>, key: ADD_CLOUD_PROVIDER}
-            ]}/>
-            <AdvancedServerParams {...this.props} />
-          </div>
-
-          <Tabs activeKey={tabKey} onChange={switchTabs} className={SessionStyles.scrollingTabCont} items={[{
-            label: t('Desired Capabilities'), key: 'new', className: SessionStyles.scrollingTab, children:
-              <CapabilityEditor {...this.props} />
-          }, {
-            label: (<span>{t('Saved Capability Sets')} <Badge count={savedSessions.length} offset={[0, -3]}/></span>),
-            key: 'saved', className: SessionStyles.scrollingTab, disabled: savedSessions.length === 0, children:
-              <SavedSessions {...this.props} />
-          }, {
-            label: t('Attach to Session'), key: 'attach', className: SessionStyles.scrollingTab, children:
-              <AttachToSession {...this.props} />
-          }]}/>
-
-          <div className={SessionStyles.sessionFooter}>
-            <div className={SessionStyles.desiredCapsLink}>
-              <a href="#" onClick={(e) => e.preventDefault() || shell.openExternal('https://appium.io/docs/en/latest/guides/caps/')}>
-                <LinkOutlined />&nbsp;
-                {t('desiredCapabilitiesDocumentation')}
-              </a>
-            </div>
-            { (!isAttaching && capsUUID) && <Button
-              onClick={() => saveSession(server, serverType, caps, {name: capsName, uuid: capsUUID})}
-              disabled={!isCapsDirty || isEditingDesiredCaps}>{t('Save')}</Button> }
-            {!isAttaching && <Button
-              onClick={requestSaveAsModal} disabled={isEditingDesiredCaps}>{t('saveAs')}</Button>}
-            {!isAttaching && <Button type={BUTTON.PRIMARY} id='btnStartSession'
-              onClick={() => newSession(caps)} className={SessionStyles['start-session-button']}>{t('startSession')}</Button>
-            }
-            {isAttaching &&
-              <Button type={BUTTON.PRIMARY} disabled={!attachSessId} onClick={() => newSession(null, attachSessId)}>
-                {t('attachToSession')}
-              </Button>
-            }
-          </div>
-
+          ]}/>
+          <AdvancedServerParams {...props} />
         </div>
-      </Spin>,
-      <CloudProviderSelector {...this.props} key='CloudProviderSelector' />
-    ];
-  }
-}
+
+        <Tabs activeKey={tabKey} onChange={switchTabs} className={SessionStyles.scrollingTabCont} items={[
+          {label: t('Desired Capabilities'),
+           key: 'new', className: SessionStyles.scrollingTab, children: <CapabilityEditor {...props} />},
+          {label: (<span>{t('Saved Capability Sets')} <Badge count={savedSessions.length} offset={[0, -3]}/></span>),
+           key: 'saved', className: SessionStyles.scrollingTab, disabled: savedSessions.length === 0, children: <SavedSessions {...props} />},
+          {label: t('Attach to Session'),
+           key: 'attach', className: SessionStyles.scrollingTab, children: <AttachToSession {...props} />}
+        ]}/>
+
+        <div className={SessionStyles.sessionFooter}>
+          <div className={SessionStyles.desiredCapsLink}>
+            <a href="#" onClick={(e) => e.preventDefault() || shell.openExternal(CAPS_DOCS_LINK)}>
+              <LinkOutlined />&nbsp;
+              {t('desiredCapabilitiesDocumentation')}
+            </a>
+          </div>
+          {(!isAttaching && capsUUID) && <Button
+            onClick={() => saveSession(server, serverType, caps, {name: capsName, uuid: capsUUID})}
+            disabled={!isCapsDirty || isEditingDesiredCaps}>{t('Save')}
+          </Button>}
+          {!isAttaching && <Button
+            onClick={requestSaveAsModal} disabled={isEditingDesiredCaps}>{t('saveAs')}
+          </Button>}
+          {!isAttaching && <Button type={BUTTON.PRIMARY} id='btnStartSession'
+            onClick={() => newSession(caps)} className={SessionStyles['start-session-button']}>{t('startSession')}
+          </Button>}
+          {isAttaching && <Button type={BUTTON.PRIMARY} disabled={!attachSessId}
+            onClick={() => newSession(null, attachSessId)}>{t('attachToSession')}
+          </Button>}
+        </div>
+
+      </div>
+    </Spin>,
+    <CloudProviderSelector {...props} key='CloudProviderSelector' />
+  ];
+};
+
+export default Session;
