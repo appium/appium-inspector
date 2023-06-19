f3ff166c11020823ee5661930bb28997bf2a5499
Edgars Eglītis <37242620+eglitise@users.noreply.github.com>
2023-06-19 11:15:06 +0300

    fix: open errormessage link in browser + move copy button to left side (#924)
    * fix: always open github link in browser window
    
    * fix: move copy btn to left side to avoid overlap

--- app/renderer/components/ErrorBoundary/ErrorMessage.js
+++ app/renderer/components/ErrorBoundary/ErrorMessage.js
@@ -4,6 +4,7 @@ import { CopyOutlined } from '@ant-design/icons';
 import styles from './ErrorMessage.css';
 import { ALERT } from '../AntdTypes';
 import { withTranslation } from '../../util';
+import { shell } from '../../polyfills';
 
 const CREATE_ISSUE_URL = 'https://github.com/appium/appium-inspector/issues/new/choose';
 
@@ -15,11 +16,12 @@ const ErrorMessage = ({ error, copyTrace, t }) => (
       showIcon
       description={
         <>
-          {t('Please report this issue at:')} <a href={CREATE_ISSUE_URL} children={CREATE_ISSUE_URL} />
+          {t('Please report this issue at:')}&nbsp;
+          <a onClick={(e) => e.preventDefault() || shell.openExternal(CREATE_ISSUE_URL)} children={CREATE_ISSUE_URL} />
           <br />
           {t('Full error trace:')}
           <Tooltip title={t('Copy Error Trace')}>
-            <Button
+            <Button size='small'
               className={styles.copyTraceBtn}
               onClick={copyTrace(error.stack)}
               icon={<CopyOutlined/>} />
