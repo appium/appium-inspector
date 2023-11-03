4e45261c43dfc1616c1e1e812557eb4a1c09e654
Edgars Eglītis <edgars.eglitis@outlook.com>
2023-11-03 12:37:15 +0200

    chore: fix unnecessary escapes

--- app/renderer/lib/client-frameworks/robot.js
+++ app/renderer/lib/client-frameworks/robot.js
@@ -1,5 +1,3 @@
-/* eslint-disable no-useless-escape */
-
 import Framework from './framework';
 import _ from 'lodash';
 
@@ -14,7 +12,7 @@ class RobotFramework extends Framework {
     if (typeof jsonVal === 'boolean') {
       return jsonVal ? '${True}' : '${False}';
     } else if (typeof jsonVal === 'number') {
-      return `\$\{${jsonVal}\}`;
+      return `$\{${jsonVal}}`;
     }
     return jsonVal;
   }
@@ -62,31 +60,31 @@ ${this.indent(code, 4)}
       return this.handleUnsupportedLocatorStrategy(strategy, locator);
     }
 
-    return `\$\{${localVar}\} =    Set Variable     ${suffixMap[strategy]}=${locator}`;
+    return `$\{${localVar}} =    Set Variable     ${suffixMap[strategy]}=${locator}`;
   }
 
   codeFor_click (varName, varIndex) {
-    return `Click Element    \$\{${this.getVarName(varName, varIndex)}\}`;
+    return `Click Element    $\{${this.getVarName(varName, varIndex)}}`;
   }
 
   codeFor_clear (varName, varIndex) {
-    return `Clear Text    \$\{${this.getVarName(varName, varIndex)}\}`;
+    return `Clear Text    $\{${this.getVarName(varName, varIndex)}}`;
   }
 
   codeFor_sendKeys (varName, varIndex, text) {
-    return `Input Text    \$\{${this.getVarName(varName, varIndex)}\}    ${text}`;
+    return `Input Text    $\{${this.getVarName(varName, varIndex)}}    ${text}`;
   }
 
   codeFor_tap (varNameIgnore, varIndexIgnore, pointerActions) {
     const {x, y} = this.getTapCoordinatesFromPointerActions(pointerActions);
-    return `@\{finger\} =    Create List    \$\{${x}\}    \$\{${y}\}
-@\{positions\} =    Create List    \$\{finger\}
-Tap With Positions    \$\{100\}    \$\{positions\}`;
+    return `@{finger} =    Create List    $\{${x}}    $\{${y}}
+@{positions} =    Create List    $\{finger}
+Tap With Positions    $\{100}    $\{positions}`;
   }
 
   codeFor_swipe (varNameIgnore, varIndexIgnore, pointerActions) {
     const {x1, y1, x2, y2} = this.getSwipeCoordinatesFromPointerActions(pointerActions);
-    return `Swipe    \$\{${x1}\}    \$\{${y1}\}    \$\{${x2}\}    \$\{${y2}\}`;
+    return `Swipe    $\{${x1}}    $\{${y1}}    $\{${x2}}    $\{${y2}}`;
   }
 
   // Execute Script
@@ -98,14 +96,14 @@ Tap With Positions    \$\{100\}    \$\{positions\}`;
   codeFor_executeScriptWithArgs (scriptCmd, jsonArg) {
     // change the JSON object into a format accepted by Create Dictionary: a sequence of key=value
     const argsValuesStrings = _.map(jsonArg[0], (v, k) => `${k}=${this.getRobotVal(v)}`);
-    return `&\{scriptArgument\}    Create Dictionary    ${argsValuesStrings.join('    ')}
-Execute Script    ${scriptCmd}    &\{scriptArgument\}`;
+    return `&{scriptArgument}    Create Dictionary    ${argsValuesStrings.join('    ')}
+Execute Script    ${scriptCmd}    &{scriptArgument}`;
   }
 
   // App Management
 
   codeFor_getCurrentActivity () {
-    return '\$\{activity_name\} =    Get Activity';
+    return '${activity_name} =    Get Activity';
   }
 
   codeFor_getCurrentPackage () {
@@ -121,7 +119,7 @@ Execute Script    ${scriptCmd}    &\{scriptArgument\}`;
   }
 
   codeFor_background (varNameIgnore, varIndexIgnore, timeout) {
-    return `Background Application    \$\{${timeout}\}`;
+    return `Background Application    $\{${timeout}}`;
   }
 
   codeFor_activateApp (varNameIgnore, varIndexIgnore, app) {
@@ -157,11 +155,11 @@ Execute Script    ${scriptCmd}    &\{scriptArgument\}`;
   }
 
   codeFor_pullFile (varNameIgnore, varIndexIgnore, pathToPullFrom) {
-    return `\$\{file_base64\} =    Pull File    ${pathToPullFrom}`;
+    return `$\{file_base64} =    Pull File    ${pathToPullFrom}`;
   }
 
   codeFor_pullFolder (varNameIgnore, varIndexIgnore, folderToPullFrom) {
-    return `\$\{folder_base64\} =    Pull Folder    ${folderToPullFrom}`;
+    return `$\{folder_base64} =    Pull Folder    ${folderToPullFrom}`;
   }
 
   // Device Interaction
@@ -171,7 +169,7 @@ Execute Script    ${scriptCmd}    &\{scriptArgument\}`;
   }
 
   codeFor_lock (varNameIgnore, varIndexIgnore, seconds) {
-    return `Lock    \$\{${seconds}\}`;
+    return `Lock    $\{${seconds}}`;
   }
 
   codeFor_unlock () {
@@ -191,7 +189,7 @@ Execute Script    ${scriptCmd}    &\{scriptArgument\}`;
   }
 
   codeFor_touchId (varNameIgnore, varIndexIgnore, match) {
-    return `Touch Id    \$\{${match}\}`;
+    return `Touch Id    $\{${match}}`;
   }
 
   codeFor_toggleEnrollTouchId () {
@@ -201,11 +199,11 @@ Execute Script    ${scriptCmd}    &\{scriptArgument\}`;
   // Keyboard
 
   codeFor_pressKeyCode (varNameIgnore, varIndexIgnore, keyCode, metaState) {
-    return `Press Keycode    \$\{${keyCode}\}    \$\{${metaState}\}`;
+    return `Press Keycode    $\{${keyCode}}    $\{${metaState}}`;
   }
 
   codeFor_longPressKeyCode (varNameIgnore, varIndexIgnore, keyCode, metaState) {
-    return `Long Press Keycode    \$\{${keyCode}\}    \$\{${metaState}\}`;
+    return `Long Press Keycode    $\{${keyCode}}    $\{${metaState}}`;
   }
 
   codeFor_hideKeyboard () {
@@ -213,7 +211,7 @@ Execute Script    ${scriptCmd}    &\{scriptArgument\}`;
   }
 
   codeFor_isKeyboardShown () {
-    return `\$\{is_keyboard_shown\} =    Is Keyboard Shown`;
+    return '${is_keyboard_shown} =    Is Keyboard Shown';
   }
 
   // Connectivity
@@ -300,7 +298,7 @@ Execute Script    ${scriptCmd}    &\{scriptArgument\}`;
   }
 
   codeFor_setGeoLocation (varNameIgnore, varIndexIgnore, latitude, longitude, altitude) {
-    return `Set Location    \$\{${latitude}\}    \$\{${longitude}\}    \$\{${altitude}\}`;
+    return `Set Location    $\{${latitude}}    $\{${longitude}}    $\{${altitude}}`;
   }
 
   codeFor_getLogTypes () {
@@ -326,7 +324,7 @@ Execute Script    ${scriptCmd}    &\{scriptArgument\}`;
   }
 
   codeFor_getUrl () {
-    return `\$\{current_url\} =    Get Window Url`;
+    return '${current_url} =    Get Window Url';
   }
 
   codeFor_back () {
@@ -344,11 +342,11 @@ Execute Script    ${scriptCmd}    &\{scriptArgument\}`;
   // Context
 
   codeFor_getContext () {
-    return `\$\{context\} =    Get Current Context`;
+    return '${context} =    Get Current Context';
   }
 
   codeFor_getContexts () {
-    return `\$\{contexts\} =    Get Contexts`;
+    return '${contexts} =    Get Contexts';
   }
 
   codeFor_switchContext (varNameIgnore, varIndexIgnore, name) {
