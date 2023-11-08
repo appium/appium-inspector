e199ed1d67af18c554bf106be3c296f56768d883
Edgars Eglītis <edgars.eglitis@outlook.com>
2023-11-08 12:16:05 +0200

    fix: copy raw session code + improve style

--- app/renderer/components/Inspector/SessionCodeBox.js
+++ app/renderer/components/Inspector/SessionCodeBox.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Card, Tooltip, Button, Select } from 'antd';
+import { Card, Tooltip, Button, Select, Space } from 'antd';
 import { CopyOutlined, CodeOutlined } from '@ant-design/icons';
 import InspectorStyles from './Inspector.css';
 import frameworks from '../../lib/client-frameworks';
@@ -8,34 +8,35 @@ import { clipboard } from '../../polyfills';
 
 const SessionCodeBox = ({ actionFramework, setActionFramework, sessionDetails, t }) => {
 
-  const code = () => {
+  const code = (raw = true) => {
     const { host, port, path, https, desiredCapabilities } = sessionDetails;
     const framework = new frameworks[actionFramework](host, port, path, https, desiredCapabilities);
     const rawCode = framework.getCodeString(true);
+    if (raw) {
+      return rawCode;
+    }
 
     return highlight(framework.language, rawCode, true).value;
   };
 
-  const actionBar = () => <div>
+  const actionBar = () => <Space size='middle'>
+    <Tooltip title={t('Copy code to clipboard')}>
+      <Button
+        icon={<CopyOutlined/>}
+        onClick={() => clipboard.writeText(code())} />
+    </Tooltip>
     <Select defaultValue={actionFramework} onChange={setActionFramework}
-      className={InspectorStyles['framework-dropdown']} size='small'>
+      className={InspectorStyles['framework-dropdown']}>
       {Object.keys(frameworks).map((f) =>
         <Select.Option value={f} key={f}>{frameworks[f].readableName}</Select.Option>
       )}
     </Select>
-    <Tooltip title={t('Copy Code to Clipboard')}>
-      <Button
-        icon={<CopyOutlined/>}
-        onClick={() => clipboard.writeText(code())}
-        type='text' />
-    </Tooltip>
-  </div>;
+  </Space>;
 
   return (
     <Card title={<span><CodeOutlined /> {t('Start this Kind of Session with Code')}</span>}
-      className={InspectorStyles['recorded-actions']}
       extra={actionBar()}>
-      <div className={InspectorStyles['recorded-code']} dangerouslySetInnerHTML={{__html: code()}} />
+      <div className={InspectorStyles['recorded-code']} dangerouslySetInnerHTML={{__html: code(false)}} />
     </Card>
   );
 };
