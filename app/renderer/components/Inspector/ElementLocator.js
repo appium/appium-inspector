2fed317345a4916f261a8d33476444932f97885e
Edgars Eglītis <edgars.eglitis@outlook.com>
2024-03-21 12:45:49 +0200

    chore: move locator strategies to constants

--- app/renderer/components/Inspector/ElementLocator.js
+++ app/renderer/components/Inspector/ElementLocator.js
@@ -3,30 +3,20 @@ import React from 'react';
 
 import {ALERT} from '../AntdTypes';
 import InspectorStyles from './Inspector.css';
-
-const STRAT_ID = ['id', 'Id'];
-const STRAT_XPATH = ['xpath', 'XPath'];
-const STRAT_NAME = ['name', 'Name'];
-const STRAT_CLASS_NAME = ['class name', 'Class Name'];
-const STRAT_ACCESSIBILITY_ID = ['accessibility id', 'Accessibility ID'];
-const STRAT_PREDICATE = ['-ios predicate string', 'Predicate String'];
-const STRAT_CLASS_CHAIN = ['-ios class chain', 'Class Chain'];
-const STRAT_UIAUTOMATOR = ['-android uiautomator', 'UIAutomator'];
-const STRAT_DATAMATCHER = ['-android datamatcher', 'DataMatcher'];
-const STRAT_VIEWTAG = ['-android viewtag', 'View Tag'];
+import {LOCATOR_STRATEGY_MAP as STRAT} from '../../constants/COMMON';
 
 const locatorStrategies = (automationName) => {
-  let strategies = [STRAT_ID, STRAT_XPATH, STRAT_NAME, STRAT_CLASS_NAME, STRAT_ACCESSIBILITY_ID];
+  let strategies = [STRAT.ID, STRAT.XPATH, STRAT.NAME, STRAT.CLASS_NAME, STRAT.ACCESSIBILITY_ID];
   switch (automationName) {
     case 'xcuitest':
     case 'mac2':
-      strategies.push(STRAT_PREDICATE, STRAT_CLASS_CHAIN);
+      strategies.push(STRAT.PREDICATE, STRAT.CLASS_CHAIN);
       break;
     case 'espresso':
-      strategies.push(STRAT_DATAMATCHER, STRAT_VIEWTAG);
+      strategies.push(STRAT.DATAMATCHER, STRAT.VIEWTAG);
       break;
     case 'uiautomator2':
-      strategies.push(STRAT_UIAUTOMATOR);
+      strategies.push(STRAT.UIAUTOMATOR);
       break;
   }
   return strategies;
