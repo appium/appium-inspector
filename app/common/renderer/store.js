9b89ebb84704429193ac2380efe305df8a33976d
Edgars Eglītis <edgars.eglitis@outlook.com>
2024-09-06 11:47:49 +0300

    chore: replace electron-extension-installer, drop redux-logger

--- app/common/renderer/store.js
+++ app/common/renderer/store.js
@@ -1,28 +1,14 @@
 import {configureStore} from '@reduxjs/toolkit';
-import {createLogger} from 'redux-logger';
 
 import actions from './actions';
 import createRootReducer from './reducers';
 
 const store = configureStore({
   reducer: createRootReducer(),
-  middleware: (getDefaultMiddleware) => {
-    const middleware = getDefaultMiddleware({
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
       serializableCheck: false,
-    });
-
-    // Additional development tools
-    if (process.env.NODE_ENV === 'development') {
-      // Logging Middleware
-      const logger = createLogger({
-        level: 'info',
-        collapsed: true,
-      });
-      middleware.push(logger);
-    }
-
-    return middleware;
-  },
+    }),
   devTools:
     process.env.NODE_ENV !== 'development'
       ? false
