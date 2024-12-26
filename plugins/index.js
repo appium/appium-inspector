cc0866fafa8cfefd4471db66efb9d3251824d826
saikrishna321 <saikrishna321@yahoo.com>
2024-12-26 18:10:11 +0530

    make inspector as appium plugin

--- plugins/index.js
+++ plugins/index.js
@@ -1,22 +1,31 @@
 import { fileURLToPath } from 'node:url';
 import { dirname } from 'node:path';
 import {BasePlugin} from 'appium/plugin.js';
-
+import express from 'express';
+import path from 'path';
 class AppiumInspectorPlugin extends BasePlugin {
   constructor(name, cliArgs) {
     super(name, cliArgs);
   }
 
   static async openInspector(req, res) {
-    // move 'dist-browser' into this directory and publish it.
-    // Then, 'appium plugin install --source=local /Users/kazu/github/appium-inspector/plugins' or local
     const reqPath = req.path === '/inspector' ? 'index.html' : req.path.substring(10);
     res.sendFile(reqPath, { root: `${dirname(fileURLToPath(import.meta.url))}/dist-browser` });
   }
 
   static async updateServer(expressApp, httpServer, cliArgs) {
+    const staticFilesRouter = express.Router();
+    const PUBLIC_DICRECTORY = getPublicDirectory();
+    staticFilesRouter.use(express.static(PUBLIC_DICRECTORY));
+    expressApp.use(staticFilesRouter);
     expressApp.all(/\/inspector.*/, AppiumInspectorPlugin.openInspector);
   }
+}
+
+function getPublicDirectory() {
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = dirname(__filename);
+  return path.join(__dirname, 'dist-browser');
 }
 
 export {AppiumInspectorPlugin};
