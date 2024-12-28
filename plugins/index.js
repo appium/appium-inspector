4ed1bd3ebc41cb9b8c6163be10969ab67a335e89
Kazuaki Matsuo <fly.49.89.over@gmail.com>
2024-12-28 09:40:40 -0800

    add const

--- plugins/index.js
+++ plugins/index.js
@@ -4,14 +4,19 @@ import {fileURLToPath} from 'node:url';
 import {BasePlugin} from '@appium/base-plugin';
 import express from 'express';
 import path from 'path';
+
+const PLUGIN_ROOT_PATH = '/inspector';
+const INDEX_HTML = 'index.html';
+const DIST_DIR = 'dist-browser';
+
 class AppiumInspectorPlugin extends BasePlugin {
   constructor(name, cliArgs) {
     super(name, cliArgs);
   }
 
   static async openInspector(req, res) {
-    const reqPath = req.path === '/inspector' ? 'index.html' : req.path.substring(10);
-    res.sendFile(reqPath, {root: `${dirname(fileURLToPath(import.meta.url))}/dist-browser`});
+    const reqPath = req.path === PLUGIN_ROOT_PATH ? INDEX_HTML : req.path.substring(10);
+    res.sendFile(reqPath, {root: `${dirname(fileURLToPath(import.meta.url))}/${DIST_DIR}`});
   }
 
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
@@ -27,7 +32,7 @@ class AppiumInspectorPlugin extends BasePlugin {
 function getPublicDirectory() {
   const __filename = fileURLToPath(import.meta.url);
   const __dirname = dirname(__filename);
-  return path.join(__dirname, 'dist-browser');
+  return path.join(__dirname, DIST_DIR);
 }
 
 export {AppiumInspectorPlugin};
