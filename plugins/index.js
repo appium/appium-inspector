7480da41776147ce9f87a63ca96f442a3cf20bcf
Kazuaki Matsuo <fly.49.89.over@gmail.com>
2024-12-29 00:51:50 -0800

    do not return inspector contents for non-/inspector route

--- plugins/index.js
+++ plugins/index.js
@@ -1,13 +1,11 @@
-import {dirname} from 'node:path';
+import path from 'node:path';
 import {fileURLToPath} from 'node:url';
 
 import {BasePlugin} from '@appium/base-plugin';
-import express from 'express';
-import path from 'path';
 
 const PLUGIN_ROOT_PATH = '/inspector';
 const INDEX_HTML = 'index.html';
-const DIST_DIR = 'dist-browser';
+const PUBLISH_ROOT_DIR = path.join(path.dirname(fileURLToPath(import.meta.url)), 'dist-browser');
 
 class AppiumInspectorPlugin extends BasePlugin {
   constructor(name, cliArgs) {
@@ -16,23 +14,18 @@ class AppiumInspectorPlugin extends BasePlugin {
 
   static async openInspector(req, res) {
     const reqPath = req.path === PLUGIN_ROOT_PATH ? INDEX_HTML : req.path.substring(10);
-    res.sendFile(reqPath, {root: `${dirname(fileURLToPath(import.meta.url))}/${DIST_DIR}`});
+    res.sendFile(reqPath, {root: PUBLISH_ROOT_DIR});
   }
 
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   static async updateServer(expressApp, httpServer, cliArgs) {
-    const staticFilesRouter = express.Router();
-    const PUBLIC_DICRECTORY = getPublicDirectory();
-    staticFilesRouter.use(express.static(PUBLIC_DICRECTORY));
-    expressApp.use(staticFilesRouter);
+    // We won't return the inspector contents for `/`
     expressApp.all(/\/inspector.*/, AppiumInspectorPlugin.openInspector);
   }
 }
 
 function getPublicDirectory() {
-  const __filename = fileURLToPath(import.meta.url);
-  const __dirname = dirname(__filename);
-  return path.join(__dirname, DIST_DIR);
+  return PUBLISH_ROOT_DIR;
 }
 
 export {AppiumInspectorPlugin};
