b1eb482dd59db178776cfad9ccc24beb4e4c75b4
saikrishna321 <saikrishna321@yahoo.com>
2024-12-31 15:33:18 +0530

    address review comments - partial
    Co-authored-by: SrinivasanTarget<srinivasan.sekar1990@gmail.com>

--- plugins/index.js
+++ plugins/index.js
@@ -1,26 +1,46 @@
 import path from 'node:path';
 import {fileURLToPath} from 'node:url';
 
-import {BasePlugin} from '@appium/base-plugin';
-
+import {BasePlugin} from '@appium/base-plugin'
 const PLUGIN_ROOT_PATH = '/inspector';
 const INDEX_HTML = 'index.html';
 const ROOT_DIR = path.join(path.dirname(fileURLToPath(import.meta.url)), 'dist-browser');
 
+/**
+ * Appium Inspector Plugin class
+ * @extends {BasePlugin}
+ */
 class AppiumInspectorPlugin extends BasePlugin {
+  /**
+   * Creates an instance of AppiumInspectorPlugin
+   * @param {string} name - The name of the plugin
+   * @param {Record<string, unknown>} cliArgs - Command line arguments
+   */
   constructor(name, cliArgs) {
     super(name, cliArgs);
   }
 
+  /**
+   * Handles inspector page requests
+   * @param {import('express').Request} req - Express request object
+   * @param {import('express').Response} res - Express response object
+   * @returns {Promise<void>}
+   */
   static async openInspector(req, res) {
     const reqPath = req.path === PLUGIN_ROOT_PATH ? INDEX_HTML : req.path.substring(10);
     res.sendFile(reqPath, {root: ROOT_DIR});
   }
 
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  /**
+   * Updates the Express server configuration
+   * @param {import('express').Application} expressApp - Express application instance
+   * @param {import('http').Server} httpServer - HTTP server instance
+   * @param {Record<string, unknown>} cliArgs - Command line arguments
+   * @returns {Promise<void>}
+   */
   static async updateServer(expressApp, httpServer, cliArgs) {
-    // We won't return the inspector contents for `/`
-    expressApp.all(/\/inspector.*/, AppiumInspectorPlugin.openInspector);
+    // Handle both /inspector and /inspector/* paths
+    expressApp.all(['/inspector', '/inspector/*'], AppiumInspectorPlugin.openInspector);
   }
 }
 
