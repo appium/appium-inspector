5f5df23e9563f2a658ad38f46ccd18d93e906c29
Kazuaki Matsuo <fly.49.89.over@gmail.com>
2024-12-31 08:21:16 -0800

    add version sync script

---
+++ scripts/sync-plugin.mjs
@@ -0,0 +1,23 @@
+import {fs} from '@appium/support';
+import path from 'node:path';
+import {fileURLToPath} from 'node:url';
+
+const ROOT_PKG_JSON_PATH = path.join(path.dirname(fileURLToPath(import.meta.url)), '..', 'package.json');
+const PLUGIN_PKG_JSON_PATH = path.join(path.dirname(fileURLToPath(import.meta.url)), '..', 'plugins', 'package.json');
+
+async function readJson(path) {
+  return await fs.readFile(path, 'utf8');
+}
+
+async function main() {
+  const rootJsonContent = await JSON.parse(await readJson(ROOT_PKG_JSON_PATH));
+  const pluginJsonContent = await JSON.parse(await readJson(PLUGIN_PKG_JSON_PATH));
+  pluginJsonContent.version = rootJsonContent.version;
+  await fs.writeFile(
+    PLUGIN_PKG_JSON_PATH,
+    JSON.stringify(pluginJsonContent, null, 2),
+    'utf8',
+  );
+}
+
+(async () => await main())();
