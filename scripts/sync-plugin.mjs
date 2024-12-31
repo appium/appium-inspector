94ec1211b44bb684ec33454c2ed3fc971aeed14f
Kazuaki Matsuo <fly.49.89.over@gmail.com>
2024-12-31 15:00:21 -0800

    tweak the script

--- scripts/sync-plugin.mjs
+++ scripts/sync-plugin.mjs
@@ -15,13 +15,18 @@ const PLUGIN_PKG_JSON_PATH = path.join(
   'package.json',
 );
 
-async function readJson(path) {
-  return await fs.readFile(path, 'utf8');
+/**
+ * Return JSON parsed contents from the given path.
+ * @param {string} path
+ * @returns {Promise<object>}
+ */
+async function readJsonContent(jsonPath) {
+  return await JSON.parse(await fs.readFile(jsonPath, 'utf8'));
 }
 
 async function main() {
-  const rootJsonContent = await JSON.parse(await readJson(ROOT_PKG_JSON_PATH));
-  const pluginJsonContent = await JSON.parse(await readJson(PLUGIN_PKG_JSON_PATH));
+  const rootJsonContent = await readJsonContent(ROOT_PKG_JSON_PATH);
+  const pluginJsonContent = await readJsonContent(PLUGIN_PKG_JSON_PATH);
   pluginJsonContent.version = rootJsonContent.version;
   await fs.writeFile(PLUGIN_PKG_JSON_PATH, JSON.stringify(pluginJsonContent, null, 2), 'utf8');
 }
