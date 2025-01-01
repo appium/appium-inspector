cb79ed3f31727360b7b42cb3a692e673718e4472
Kazuaki Matsuo <fly.49.89.over@gmail.com>
2025-01-01 15:09:21 -0800

    simplified a bit

--- plugins/index.js
+++ plugins/index.js
@@ -4,7 +4,7 @@ import {fileURLToPath} from 'node:url';
 import {BasePlugin} from '@appium/base-plugin';
 const PLUGIN_ROOT_PATH = '/inspector';
 const INDEX_HTML = 'index.html';
-const ROOT_DIR = path.join(path.dirname(fileURLToPath(import.meta.url)), 'dist-browser');
+const ROOT_DIR = path.resolve(path.dirname(fileURLToPath(import.meta.url)), 'dist-browser');
 
 /**
  * Appium Inspector Plugin class
@@ -27,19 +27,16 @@ class AppiumInspectorPlugin extends BasePlugin {
    * @returns {Promise<void>}
    */
   static async openInspector(req, res) {
-    const reqPath = req.path === PLUGIN_ROOT_PATH ? INDEX_HTML : req.path.substring(10);
+    const reqPath = req.path === PLUGIN_ROOT_PATH ? INDEX_HTML : req.path.substring(PLUGIN_ROOT_PATH.length);
     res.sendFile(reqPath, {root: ROOT_DIR});
   }
 
   /**
    * Updates the Express server configuration
    * @param {import('express').Application} expressApp - Express application instance
-   * @param {import('http').Server} httpServer - HTTP server instance
-   * @param {Record<string, unknown>} cliArgs - Command line arguments
    * @returns {Promise<void>}
    */
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  static async updateServer(expressApp, httpServer, cliArgs) {
+  static async updateServer(expressApp) {
     // Handle both /inspector and /inspector/* paths
     expressApp.all(['/inspector', '/inspector/*'], AppiumInspectorPlugin.openInspector);
   }
