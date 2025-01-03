adfa60ed79be9f74d0a2fcbac685eca083979453
Edgars Eglītis <edgars.eglitis@outlook.com>
2025-01-03 12:52:34 +0200

    chore: bump ESLint-related packages

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,9 +1,8 @@
 import path from 'node:path';
 import {fileURLToPath} from 'node:url';
 
+import appiumConfig from '@appium/eslint-config-appium-ts';
 import {includeIgnoreFile} from '@eslint/compat';
-import {FlatCompat} from '@eslint/eslintrc';
-import js from '@eslint/js';
 import reactPlugin from 'eslint-plugin-react';
 import simpleImportSortPlugin from 'eslint-plugin-simple-import-sort';
 import globals from 'globals';
@@ -11,20 +10,15 @@ import globals from 'globals';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const gitignorePath = path.resolve(__dirname, '.gitignore');
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-  recommendedConfig: js.configs.recommended,
-  allConfig: js.configs.all,
-});
 
 export default [
+  ...appiumConfig,
   {
     name: 'React Plugin',
     files: ['**/*.{js,mjs,cjs,jsx,mjsx,ts,tsx,mtsx}'],
     ...reactPlugin.configs.flat.recommended,
     ...reactPlugin.configs.flat['jsx-runtime'],
   },
-  ...compat.extends('@appium/eslint-config-appium-ts'),
   {
     name: 'JS/TS Files',
     files: ['**/*.{js,mjs,cjs,jsx,mjsx,ts,tsx,mtsx}'],
