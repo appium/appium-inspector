a65a953dd46f8f612c86455dca9a5be21b4ebe58
Kazuaki Matsuo <fly.49.89.over@gmail.com>
2025-01-09 01:10:27 -0800

    add deps of @appium/base-plugin

--- plugins/index.mjs
+++ plugins/index.mjs
@@ -1,7 +1,7 @@
 import path from 'node:path';
 import {fileURLToPath} from 'node:url';
 
-import {BasePlugin} from 'appium/plugin.js';
+import {BasePlugin} from '@appium/base-plugin';
 const PLUGIN_ROOT_PATH = '/inspector';
 const INDEX_HTML = 'index.html';
 const ROOT_DIR = path.resolve(path.dirname(fileURLToPath(import.meta.url)), 'dist-browser');
