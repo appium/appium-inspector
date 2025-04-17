27dd17f57da84b9fa4b05e4874718a18835a42dd
losingle <losingle@gmail.com>
2025-04-17 23:28:04 +0800

    Fix lint issues in Session component

--- app/common/renderer/components/Session/Session.jsx
+++ app/common/renderer/components/Session/Session.jsx
@@ -4,16 +4,17 @@ import _ from 'lodash';
 import {useEffect} from 'react';
 import {useNavigate} from 'react-router';
 
-import {ADD_CLOUD_PROVIDER_TAB_KEY, SERVER_TYPES, SESSION_BUILDER_TABS} from '../../constants/session-builder';
 import {BUTTON} from '../../constants/antd-types';
 import {LINKS} from '../../constants/common';
+import {ADD_CLOUD_PROVIDER_TAB_KEY, SERVER_TYPES, SESSION_BUILDER_TABS} from '../../constants/session-builder';
 import {ipcRenderer, openLink} from '../../polyfills';
 import {log} from '../../utils/logger';
+
 import AdvancedServerParams from './AdvancedServerParams.jsx';
 import AttachToSession from './AttachToSession.jsx';
 import CapabilityEditor from './CapabilityEditor.jsx';
-import CloudProviders from './CloudProviders.jsx';
 import CloudProviderSelector from './CloudProviderSelector.jsx';
+import CloudProviders from './CloudProviders.jsx';
 import SavedSessions from './SavedSessions.jsx';
 import ServerTabCustom from './ServerTabCustom.jsx';
 import SessionStyles from './Session.module.css';
