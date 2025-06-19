0739fb4fe11eedb23134d3c8f22d17255fb30d7e
William Husar <william.husar@hotmail.com>
2025-06-19 08:27:07 +0200

    prettier

--- app/common/renderer/providers/ThemeProvider.jsx
+++ app/common/renderer/providers/ThemeProvider.jsx
@@ -1,13 +1,13 @@
-import { App, ConfigProvider, Layout, theme } from 'antd';
-import { createContext, useEffect, useState } from 'react';
+import {App, ConfigProvider, Layout, theme} from 'antd';
+import {createContext, useEffect, useState} from 'react';
 
-import { PREFERRED_THEME } from '../../shared/setting-defs';
-import { getSetting, setSetting } from '../polyfills';
-import { loadHighlightTheme } from '../utils/highlight-theme';
+import {PREFERRED_THEME} from '../../shared/setting-defs';
+import {getSetting, setSetting} from '../polyfills';
+import {loadHighlightTheme} from '../utils/highlight-theme';
 
 export const ThemeContext = createContext(undefined);
 
-export const ThemeProvider = ({ children }) => {
+export const ThemeProvider = ({children}) => {
   const [isDarkTheme, setIsDarkTheme] = useState(false);
   loadHighlightTheme(isDarkTheme);
 
@@ -40,7 +40,7 @@ export const ThemeProvider = ({ children }) => {
   };
 
   return (
-    <ThemeContext.Provider value={{ toggleTheme, isDarkTheme }}>
+    <ThemeContext.Provider value={{toggleTheme, isDarkTheme}}>
       <ConfigProvider theme={themeConfig}>
         <App>
           <Layout data-theme={isDarkTheme ? 'dark' : 'light'}>{children}</Layout>
