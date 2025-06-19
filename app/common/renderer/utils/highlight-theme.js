d278fca633144524843b5cfcebd277af8eb77dcc
William Husar <william.husar@hotmail.com>
2025-06-19 11:03:34 +0200

    replace highlight theme if exists

--- app/common/renderer/utils/highlight-theme.js
+++ app/common/renderer/utils/highlight-theme.js
@@ -2,15 +2,20 @@ import darkTheme from 'highlight.js/styles/atom-one-dark.css?url';
 import lightTheme from 'highlight.js/styles/intellij-light.css?url';
 
 export const loadHighlightTheme = (isDarkTheme) => {
-  const link = document.createElement('link');
-  link.rel = 'stylesheet';
-  link.type = 'text/css';
+  const linkId = 'highlight-theme';
+  let link = document.getElementById(linkId);
+
+  if (!link) {
+    link = document.createElement('link');
+    link.id = linkId;
+    link.rel = 'stylesheet';
+    link.type = 'text/css';
+    document.head.appendChild(link);
+  }
 
   if (isDarkTheme) {
     link.href = darkTheme;
   } else {
     link.href = lightTheme;
   }
-
-  document.head.appendChild(link);
 };
