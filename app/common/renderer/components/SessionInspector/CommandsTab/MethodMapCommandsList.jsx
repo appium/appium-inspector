a8d1b7710028b7ed6adcf03e662b83a910606630
Edgars Eglītis <edgars.eglitis@outlook.com>
2025-10-21 13:32:38 +0300

    add UI for rendering command/method buttons

--- app/common/renderer/components/SessionInspector/CommandsTab/MethodMapCommandsList.jsx
+++ app/common/renderer/components/SessionInspector/CommandsTab/MethodMapCommandsList.jsx
@@ -1,5 +1,49 @@
-import {Tabs} from 'antd';
+import {Button, Col, Collapse, Row, Tabs} from 'antd';
 import _ from 'lodash';
+
+import styles from './Commands.module.css';
+
+const CommandsButtonGrid = ({restDriverCommands}) => (
+  <Collapse
+    items={_.toPairs(restDriverCommands).map(([commandSource, commandPathsToMethodsMap]) => ({
+      key: commandSource,
+      label: _.capitalize(commandSource),
+      children: (
+        <Row>
+          {Object.values(commandPathsToMethodsMap).map((commandMethodsMap) =>
+            Object.values(commandMethodsMap).map((commandItem, index) => (
+              <Col key={index} xs={12} sm={12} md={12} lg={8} xl={6} xxl={4}>
+                <div className={styles.btnContainer}>
+                  <Button>{commandItem.command}</Button>
+                </div>
+              </Col>
+            )),
+          )}
+        </Row>
+      ),
+    }))}
+  />
+);
+
+const ExecuteMethodsButtonGrid = ({executeMethods}) => (
+  <Collapse
+    items={_.toPairs(executeMethods).map(([methodSource, methodMap]) => ({
+      key: methodSource,
+      label: _.capitalize(methodSource),
+      children: (
+        <Row>
+          {_.toPairs(methodMap).map(([methodName], index) => (
+            <Col key={index} xs={12} sm={12} md={12} lg={8} xl={6} xxl={4}>
+              <div className={styles.btnContainer}>
+                <Button>{methodName}</Button>
+              </div>
+            </Col>
+          ))}
+        </Row>
+      ),
+    }))}
+  />
+);
 
 // Dynamic list of driver commands, generated from the driver's method map responses
 const MethodMapCommandsList = (props) => {
@@ -18,13 +62,13 @@ const MethodMapCommandsList = (props) => {
           label: t('Commands'),
           key: '1',
           disabled: hasNoCommands,
-          children: 'Commands',
+          children: <CommandsButtonGrid restDriverCommands={driverCommands.rest} />,
         },
         {
           label: t('executeMethods'),
           key: '2',
           disabled: hasNoExecuteMethods,
-          children: 'Execute Methods',
+          children: <ExecuteMethodsButtonGrid executeMethods={driverExecuteMethods.rest} />,
         },
       ]}
     />
