9275a34e1ef84e5d36794b60d89de94528935565
Edgars Eglītis <edgars.eglitis@outlook.com>
2025-10-28 15:25:33 +0200

    refactor MethodMapCommandsList components

--- app/common/renderer/components/SessionInspector/CommandsTab/MethodMapCommandsList.jsx
+++ app/common/renderer/components/SessionInspector/CommandsTab/MethodMapCommandsList.jsx
@@ -16,69 +16,61 @@ const renderCommandTooltipText = (methodDetails, t) => {
   );
 };
 
-const MethodMapButtonsGrid = ({driverCommands, startCommand, isExecute, t}) => {
-  const InnerGrid = ({methodMap}) => (
-    <Row>
-      {_.toPairs(methodMap).map(([methodName, methodDetails], index) => (
-        <Col key={index} xs={12} sm={12} md={12} lg={8} xl={6} xxl={4}>
-          <div className={styles.btnContainer}>
-            <Tooltip title={renderCommandTooltipText(methodDetails, t)}>
-              <Button
-                className={methodDetails.deprecated ? styles.deprecatedMethod : ''}
-                onClick={() => startCommand({name: methodName, details: methodDetails, isExecute})}
-              >
-                {methodName}
-              </Button>
-            </Tooltip>
-          </div>
-        </Col>
-      ))}
-    </Row>
-  );
-
-  return _.size(driverCommands) === 1 ? (
-    <InnerGrid methodMap={Object.values(driverCommands)[0]} />
-  ) : (
-    <Collapse
-      items={_.toPairs(driverCommands).map(([methodSource, methodMap]) => ({
-        key: methodSource,
-        label: _.capitalize(methodSource),
-        children: <InnerGrid methodMap={methodMap} />,
-      }))}
-    />
-  );
-};
-
-const CommandsButtonGrid = ({driverCommands, startCommand, t}) => (
-  <Space className={inspectorStyles.spaceContainer} direction="vertical" size="middle">
-    {t('dynamicCommandsDescription')}
-    <MethodMapButtonsGrid
-      driverCommands={driverCommands}
-      startCommand={startCommand}
-      isExecute={false}
-      t={t}
-    />
-  </Space>
-);
-
-const ExecuteMethodsButtonGrid = ({executeMethods, startCommand, t}) => (
-  <Space className={inspectorStyles.spaceContainer} direction="vertical" size="middle">
-    {t('dynamicExecuteMethodsDescription')}
-    <MethodMapButtonsGrid
-      driverCommands={executeMethods}
-      startCommand={startCommand}
-      isExecute={true}
-      t={t}
-    />
-  </Space>
-);
-
 // Dynamic list of driver commands, generated from the driver's method map responses
 const MethodMapCommandsList = (props) => {
   const {driverCommands, driverExecuteMethods, startCommand, t} = props;
 
   const hasNoCommands = _.isEmpty(driverCommands);
   const hasNoExecuteMethods = _.isEmpty(driverExecuteMethods);
+
+  const MethodMapButtonsGrid = ({driverCommands, isExecute}) => {
+    const InnerGrid = ({methodMap}) => (
+      <Row>
+        {_.toPairs(methodMap).map(([methodName, methodDetails], index) => (
+          <Col key={index} xs={12} sm={12} md={12} lg={8} xl={6} xxl={4}>
+            <div className={styles.btnContainer}>
+              <Tooltip title={renderCommandTooltipText(methodDetails, t)}>
+                <Button
+                  className={methodDetails.deprecated ? styles.deprecatedMethod : ''}
+                  onClick={() =>
+                    startCommand({name: methodName, details: methodDetails, isExecute})
+                  }
+                >
+                  {methodName}
+                </Button>
+              </Tooltip>
+            </div>
+          </Col>
+        ))}
+      </Row>
+    );
+
+    return _.size(driverCommands) === 1 ? (
+      <InnerGrid methodMap={Object.values(driverCommands)[0]} />
+    ) : (
+      <Collapse
+        items={_.toPairs(driverCommands).map(([methodSource, methodMap]) => ({
+          key: methodSource,
+          label: _.capitalize(methodSource),
+          children: <InnerGrid methodMap={methodMap} />,
+        }))}
+      />
+    );
+  };
+
+  const CommandsButtonGrid = () => (
+    <Space className={inspectorStyles.spaceContainer} direction="vertical" size="middle">
+      {t('dynamicCommandsDescription')}
+      <MethodMapButtonsGrid driverCommands={driverCommands} isExecute={false} />
+    </Space>
+  );
+
+  const ExecuteMethodsButtonGrid = () => (
+    <Space className={inspectorStyles.spaceContainer} direction="vertical" size="middle">
+      {t('dynamicExecuteMethodsDescription')}
+      <MethodMapButtonsGrid driverCommands={driverExecuteMethods.rest} isExecute={true} />
+    </Space>
+  );
 
   return (
     <Tabs
@@ -90,21 +82,13 @@ const MethodMapCommandsList = (props) => {
           label: t('Commands'),
           key: '1',
           disabled: hasNoCommands,
-          children: (
-            <CommandsButtonGrid driverCommands={driverCommands} startCommand={startCommand} t={t} />
-          ),
+          children: <CommandsButtonGrid />,
         },
         {
           label: t('executeMethods'),
           key: '2',
           disabled: hasNoExecuteMethods,
-          children: (
-            <ExecuteMethodsButtonGrid
-              executeMethods={driverExecuteMethods.rest}
-              startCommand={startCommand}
-              t={t}
-            />
-          ),
+          children: <ExecuteMethodsButtonGrid />,
         },
       ]}
     />
