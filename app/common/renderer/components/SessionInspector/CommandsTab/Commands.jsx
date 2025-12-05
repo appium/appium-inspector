d21f56adf6a0bbd5bb12211ab686dfbc777fb71b
Edgars Eglītis <edgars.eglitis@outlook.com>
2025-12-05 12:24:53 +0200

    chore: replace addonBefore in Commands

--- app/common/renderer/components/SessionInspector/CommandsTab/Commands.jsx
+++ app/common/renderer/components/SessionInspector/CommandsTab/Commands.jsx
@@ -1,5 +1,5 @@
 import {ThunderboltOutlined} from '@ant-design/icons';
-import {Card, Col, Input, Modal, Row, Typography} from 'antd';
+import {Card, Input, Modal, Row, Space, Typography} from 'antd';
 import _ from 'lodash';
 import {useEffect, useRef, useState} from 'react';
 
@@ -21,7 +21,7 @@ const formatParamInputLabel = (param) => {
   if (param.required) {
     return (
       <>
-        <Typography.Text type="danger">*</Typography.Text> {param.name}
+        <Typography.Text type="danger">*</Typography.Text>&nbsp;{param.name}
       </>
     );
   }
@@ -146,12 +146,10 @@ const Commands = (props) => {
           >
             {_.map(curCommandDetails.details.params, (param, index) => (
               <Row key={index} gutter={16}>
-                <Col span={24} className={styles.argContainer}>
-                  <Input
-                    addonBefore={formatParamInputLabel(param)}
-                    onChange={(e) => (curCommandParamVals.current[index] = e.target.value)}
-                  />
-                </Col>
+                <Space.Compact block className={styles.commandArgInputRow}>
+                  <Space.Addon>{formatParamInputLabel(param)}</Space.Addon>
+                  <Input onChange={(e) => (curCommandParamVals.current[index] = e.target.value)} />
+                </Space.Compact>
               </Row>
             ))}
           </Modal>
