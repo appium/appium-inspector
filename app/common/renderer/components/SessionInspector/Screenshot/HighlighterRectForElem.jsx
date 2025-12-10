6105aaa43b78bbf79ac471a5ba3262ae3b433e49
Edgars Eglītis <edgars.eglitis@outlook.com>
2025-12-10 09:37:14 +0200

    prevent re-rendering highlighter rects on hover

--- app/common/renderer/components/SessionInspector/Screenshot/HighlighterRectForElem.jsx
+++ app/common/renderer/components/SessionInspector/Screenshot/HighlighterRectForElem.jsx
@@ -5,36 +5,20 @@ import styles from './Screenshot.module.css';
  * boxes of the elements in the app
  */
 const HighlighterRectForElem = (props) => {
-  const {
-    hoveredElementPath,
-    selectHoveredElement,
-    unselectHoveredElement,
-    selectedElementPath,
-    selectElement,
-    unselectElement,
-    dimensions,
-    element,
-  } = props;
+  const {selectedElementPath, selectElement, unselectElement, properties, path} = props;
 
-  const {width, height, left, top} = dimensions;
-  const key = element.path;
-  let highlighterClasses = [styles.highlighterBox];
+  const {width, height, left, top} = properties;
+  const highlighterClasses = [styles.highlighterBox];
 
-  // Add class + special classes to hovered and selected elements
-  if (hoveredElementPath === element.path) {
-    highlighterClasses.push(styles.hoveredElementBox);
-  }
-  if (selectedElementPath === element.path) {
+  // Add class to selected elements
+  if (selectedElementPath === path) {
     highlighterClasses.push(styles.inspectedElementBox);
   }
 
   return (
     <div
       className={highlighterClasses.join(' ').trim()}
-      onMouseOver={() => selectHoveredElement(key)}
-      onMouseOut={unselectHoveredElement}
-      onClick={() => (key === selectedElementPath ? unselectElement() : selectElement(key))}
-      key={key}
+      onClick={() => (path === selectedElementPath ? unselectElement() : selectElement(path))}
       style={{left: left || 0, top: top || 0, width: width || 0, height: height || 0}}
     >
       <div></div>
