40019819f9a9670aeffd8bdd67a8dd1af7e0ca39
Edgars Eglītis <37242620+eglitise@users.noreply.github.com>
2026-01-15 12:37:58 +0200

    chore: merge Root and index files (#2560)

--- app/common/renderer/index.jsx
+++ app/common/renderer/index.jsx
@@ -1,14 +1,31 @@
+import {Suspense} from 'react';
 import {createRoot} from 'react-dom/client';
+import {Provider} from 'react-redux';
+import {MemoryRouter, Route, Routes} from 'react-router';
 
 import ErrorBoundary from './components/ErrorBoundary/ErrorBoundary.jsx';
-import Root from './Root.jsx';
+import Spinner from './components/Spinner/Spinner.jsx';
+import SessionBuilderPage from './containers/SessionBuilderPage.js';
+import SessionInspectorPage from './containers/SessionInspectorPage.js';
+import {ThemeProvider} from './providers/ThemeProvider.jsx';
 import store from './store.js';
 
 const container = document.getElementById('root');
-const root = createRoot(container);
 
-root.render(
+createRoot(container).render(
   <ErrorBoundary>
-    <Root store={store} />
+    <Provider store={store}>
+      <ThemeProvider>
+        <MemoryRouter initialEntries={['/']}>
+          <Suspense fallback={<Spinner />}>
+            <Routes>
+              <Route path="/" element={<SessionBuilderPage />} />
+              <Route path="/session" element={<SessionBuilderPage />} />
+              <Route path="/inspector" element={<SessionInspectorPage />} />
+            </Routes>
+          </Suspense>
+        </MemoryRouter>
+      </ThemeProvider>
+    </Provider>
   </ErrorBoundary>,
 );
