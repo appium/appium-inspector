462ee3a48cfb7df0af9e3b58f12124c1e27e5eaf
Taha Tangülü <tangulutaha@gmail.com>
2026-02-06 02:29:09 +0300

    feat: add RabbitQA cloud provider support with logo and configuration

--- README.md
+++ README.md
@@ -208,6 +208,11 @@ The Inspector has built-in integrations with various cloud service provider plat
         </picture>
       </a>
     </td>
+    <td align="center" valign="center">
+      <a href="https://docs.rabbitqa.com/">
+        <img width="200" height="50" alt="RabbitQA" src="app/common/renderer/assets/images/rabbitqa_logo.svg">
+      </a>
+    </td>
   </tr>
 </table>
 
---
+++ app/common/renderer/lib/vendor/rabbitqa.js
@@ -0,0 +1,24 @@
+import {BaseVendor} from './base.js';
+
+export class RabbitQAVendor extends BaseVendor {
+  /**
+   * @override
+   */
+  async configureProperties() {
+    const rabbitQA = this._server.rabbitqa;
+    const vendorName = 'RabbitQA';
+
+    const apiKey = rabbitQA?.apiKey || process.env.RABBITQA_API_KEY;
+
+    this._checkInputPropertyPresence(vendorName, [{name: 'API Key', val: apiKey}]);
+
+    const host = 'api.rabbitqa.com';
+    const port = 443;
+    const path = '/df/wd/hub';
+    const https = true;
+
+    this._saveProperties(rabbitQA, {host, path, port, https, accessKey: apiKey});
+
+    this._updateSessionCap('df:apiKey', apiKey, false);
+  }
+}
