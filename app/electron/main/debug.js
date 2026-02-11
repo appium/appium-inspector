d0f9cb54c83874a14f9f65b2fa4af89a148bebd6
Edgars Eglītis <37242620+eglitise@users.noreply.github.com>
2026-02-11 23:49:37 +0200

    chore: improve logging when installing dev extensions (#2649)
    * chore: improve logging when installing dev extensions
    
    * prevent error if no extensions are installed (not actually possible)
    
    * use node:path to get extensions directory
    
    * log message in case of error

--- app/electron/main/debug.js
+++ app/electron/main/debug.js
@@ -1,12 +1,46 @@
-import {installExtension, REACT_DEVELOPER_TOOLS, REDUX_DEVTOOLS} from 'electron-devtools-installer';
+import {join} from 'node:path';
+
+import {
+  installExtension,
+  REACT_DEVELOPER_TOOLS,
+  REDUX_DEVTOOLS,
+} from '@tomjs/electron-devtools-installer';
+
+const EXTENSIONS_TO_INSTALL = [REACT_DEVELOPER_TOOLS, REDUX_DEVTOOLS];
+
+// These console.log wrappers are used for better visual separation from other logs
+
+function logEmptyLine() {
+  console.log(''); // eslint-disable-line no-console
+}
+
+function logWithExtPrefix(textString) {
+  console.log(`[🧩 Extensions 🧩] ${textString}`); // eslint-disable-line no-console
+}
 
 export async function installExtensions() {
   const opts = {
     forceDownload: !!process.env.UPGRADE_EXTENSIONS,
   };
+  logEmptyLine();
+  logWithExtPrefix('Installing development extensions...');
+  logWithExtPrefix(
+    opts.forceDownload
+      ? 'Explicitly re-downloading all extensions'
+      : 'Set UPGRADE_EXTENSIONS=1 to force re-download',
+  );
   try {
-    await installExtension([REACT_DEVELOPER_TOOLS, REDUX_DEVTOOLS], opts);
+    const installedExts = await installExtension(EXTENSIONS_TO_INSTALL, opts);
+    if (installedExts.length === 0) {
+      logWithExtPrefix('No extensions were installed');
+    } else {
+      logWithExtPrefix(`Installed extensions at ${join(installedExts[0].path, '..')}:`);
+      for (const ext of installedExts) {
+        logWithExtPrefix(`* ${ext.name} v${ext.version} (ID: ${ext.id})`);
+      }
+    }
   } catch (e) {
-    console.warn(`Error installing extension: ${e}`); // eslint-disable-line no-console
+    logWithExtPrefix(`Error installing extensions: ${e.message}`);
   }
+  logEmptyLine();
 }
