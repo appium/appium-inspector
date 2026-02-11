c87502cfc2a56b5338e3ef8f9cf1993eabe4b4e3
Edgars Eglītis <edgars.eglitis@outlook.com>
2026-02-11 20:32:15 +0200

    Windows still ok, revert temp changes

--- app/electron/main/main.js
+++ app/electron/main/main.js
@@ -23,9 +23,9 @@ app.on('window-all-closed', () => {
 app.on('ready', async () => {
   if (isDev) {
     debug();
+    await installExtensions();
   }
-  await installExtensions();
-  app.quit();
-  // setupIPCListeners();
-  // await setupMainWindow();
+
+  setupIPCListeners();
+  await setupMainWindow();
 });
