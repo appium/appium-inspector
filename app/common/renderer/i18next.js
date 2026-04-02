4add5ea5cb6f632658853353deda475b33a786d0
Edgars Eglītis <edgars.eglitis@outlook.com>
2026-04-02 16:35:13 +0300

    remove unneeded i18next backends the caching only brings issues, and all hosting is local only anyway

--- app/common/renderer/i18next.js
+++ app/common/renderer/i18next.js
@@ -1,7 +1,5 @@
 import i18n from 'i18next';
-import i18NextBackend from 'i18next-chained-backend';
 import HttpApi from 'i18next-http-backend';
-import LocalStorageBackend from 'i18next-localstorage-backend';
 import {initReactI18next} from 'react-i18next';
 
 import {commonI18NextOptions} from '../shared/i18next.config.js';
@@ -12,18 +10,12 @@ const i18nextOptions = {
   ...commonI18NextOptions,
   lng: await getSetting(PREFERRED_LANGUAGE),
   backend: {
-    backends: [LocalStorageBackend, HttpApi],
-    backendOptions: [
-      {},
-      {
-        loadPath: `${localesPath}/{{lng}}/{{ns}}.json`,
-      },
-    ],
+    loadPath: `${localesPath}/{{lng}}/{{ns}}.json`,
   },
 };
 
 if (!i18n.isInitialized) {
-  i18n.use(initReactI18next).use(i18NextBackend).init(i18nextOptions);
+  i18n.use(HttpApi).use(initReactI18next).init(i18nextOptions);
 }
 
 export default i18n;
