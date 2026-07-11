92eabded234e8c1e407b6d0f65ab72eba84eb8d0
Edgars Eglītis <edgars.eglitis@outlook.com>
2026-07-11 17:19:53 +0300

    chore: split general controls group more

--- app/common/renderer/components/SessionInspector/Header/GeneralControlsGroup.jsx
+++ app/common/renderer/components/SessionInspector/Header/GeneralControlsGroup.jsx
@@ -11,6 +11,83 @@ import {useTranslation} from 'react-i18next';
 import {BUTTON} from '../../../constants/antd-types.js';
 import LocatorSearchModal from './LocatorSearch/LocatorSearchModal.jsx';
 
+const RefreshControlsGroup = ({
+  isUsingMjpegMode,
+  isSourceRefreshOn,
+  setRefreshingState,
+  applyClientMethod,
+}) => {
+  const {t} = useTranslation();
+
+  return (
+    <>
+      {isUsingMjpegMode &&
+        (isSourceRefreshOn ? (
+          <Tooltip title={t('Pause Refreshing Source')}>
+            <Button
+              id="btnPauseRefreshing"
+              icon={<IconPlayerPause size={18} />}
+              onClick={() => setRefreshingState({source: false})}
+            />
+          </Tooltip>
+        ) : (
+          <Tooltip title={t('Start Refreshing Source')}>
+            <Button
+              id="btnStartRefreshing"
+              icon={<IconPlayerPlay size={18} />}
+              onClick={() => setRefreshingState({source: true})}
+            />
+          </Tooltip>
+        ))}
+      <Tooltip title={t('refreshSource')}>
+        <Button
+          id="btnReload"
+          icon={<IconRefresh size={18} />}
+          onClick={() => applyClientMethod({methodName: 'getPageSource'})}
+        />
+      </Tooltip>
+    </>
+  );
+};
+
+const SearchControlsGroup = (props) => {
+  const {showLocatorSearchModal} = props;
+  const {t} = useTranslation();
+
+  return (
+    <>
+      <Tooltip title={t('Search for element')}>
+        <Button
+          id="searchForElement"
+          icon={<IconSearch size={18} />}
+          onClick={showLocatorSearchModal}
+        />
+      </Tooltip>
+      <LocatorSearchModal {...props} />
+    </>
+  );
+};
+
+const RecordingControlsGroup = ({isRecording, startRecording, pauseRecording}) => {
+  const {t} = useTranslation();
+
+  return isRecording ? (
+    <Tooltip title={t('Pause Recording')}>
+      <Button
+        id="btnPause"
+        icon={<IconVideo size={18} />}
+        type={BUTTON.PRIMARY}
+        danger
+        onClick={pauseRecording}
+      />
+    </Tooltip>
+  ) : (
+    <Tooltip title={t('Start Recording')}>
+      <Button id="btnStartRecording" icon={<IconVideo size={18} />} onClick={startRecording} />
+    </Tooltip>
+  );
+};
+
 /**
  * Controls for general session actions.
  */
@@ -20,64 +97,24 @@ const GeneralControlsGroup = (props) => {
     isSourceRefreshOn,
     setRefreshingState,
     applyClientMethod,
-    showLocatorSearchModal,
     isRecording,
     startRecording,
     pauseRecording,
   } = props;
-  const {t} = useTranslation();
-
   return (
     <Space.Compact>
-      {isUsingMjpegMode && !isSourceRefreshOn && (
-        <Tooltip title={t('Start Refreshing Source')}>
-          <Button
-            id="btnStartRefreshing"
-            icon={<IconPlayerPlay size={18} />}
-            onClick={() => setRefreshingState({source: true})}
-          />
-        </Tooltip>
-      )}
-      {isUsingMjpegMode && isSourceRefreshOn && (
-        <Tooltip title={t('Pause Refreshing Source')}>
-          <Button
-            id="btnPauseRefreshing"
-            icon={<IconPlayerPause size={18} />}
-            onClick={() => setRefreshingState({source: false})}
-          />
-        </Tooltip>
-      )}
-      <Tooltip title={t('refreshSource')}>
-        <Button
-          id="btnReload"
-          icon={<IconRefresh size={18} />}
-          onClick={() => applyClientMethod({methodName: 'getPageSource'})}
-        />
-      </Tooltip>
-      <Tooltip title={t('Search for element')}>
-        <Button
-          id="searchForElement"
-          icon={<IconSearch size={18} />}
-          onClick={showLocatorSearchModal}
-        />
-      </Tooltip>
-      <LocatorSearchModal {...props} />
-      {!isRecording && (
-        <Tooltip title={t('Start Recording')}>
-          <Button id="btnStartRecording" icon={<IconVideo size={18} />} onClick={startRecording} />
-        </Tooltip>
-      )}
-      {isRecording && (
-        <Tooltip title={t('Pause Recording')}>
-          <Button
-            id="btnPause"
-            icon={<IconVideo size={18} />}
-            type={BUTTON.PRIMARY}
-            danger
-            onClick={pauseRecording}
-          />
-        </Tooltip>
-      )}
+      <RefreshControlsGroup
+        isUsingMjpegMode={isUsingMjpegMode}
+        isSourceRefreshOn={isSourceRefreshOn}
+        setRefreshingState={setRefreshingState}
+        applyClientMethod={applyClientMethod}
+      />
+      <SearchControlsGroup {...props} />
+      <RecordingControlsGroup
+        isRecording={isRecording}
+        startRecording={startRecording}
+        pauseRecording={pauseRecording}
+      />
     </Space.Compact>
   );
 };
