d6ecd4eefe4f832a28c6a2394bfedf9a3b369570
Edgars Eglītis <edgars.eglitis@outlook.com>
2026-07-11 16:49:38 +0300

    chore: split context controls button group

--- app/common/renderer/components/SessionInspector/Header/HeaderButtons.jsx
+++ app/common/renderer/components/SessionInspector/Header/HeaderButtons.jsx
@@ -2,21 +2,17 @@ import {
   IconCarouselHorizontal,
   IconChevronLeft,
   IconCircle,
-  IconExclamationCircle,
   IconHome,
-  IconInfoCircle,
   IconMessageChatbot,
   IconSquare,
-  IconTriangleSquareCircle,
-  IconWorld,
 } from '@tabler/icons-react';
 import {Button, Divider, Select, Space, Tooltip} from 'antd';
 import {useTranslation} from 'react-i18next';
 
 import {BUTTON} from '../../../constants/antd-types.js';
-import {DRIVERS, LINKS} from '../../../constants/common.js';
-import {APP_MODE} from '../../../constants/session-inspector.js';
+import {DRIVERS} from '../../../constants/common.js';
 import {openLink} from '../../../polyfills.js';
+import ContextControlsGroup from './ElementGroups/ContextControlsGroup.jsx';
 import GeneralControlsGroup from './ElementGroups/GeneralControlsGroup.jsx';
 import SessionQuitControlsGroup from './ElementGroups/SessionQuitControlsGroup.jsx';
 import SessionReloadButton from './ElementGroups/SessionReloadButton.jsx';
@@ -141,74 +137,20 @@ const HeaderButtons = (props) => {
     </Space.Compact>
   );
 
-  const appModeControls = (
-    <Space.Compact>
-      <Tooltip title={t('Native App Mode')}>
-        <Button
-          icon={<IconTriangleSquareCircle size={18} />}
-          onClick={() => selectAppMode(APP_MODE.NATIVE)}
-          type={appMode === APP_MODE.NATIVE ? BUTTON.PRIMARY : BUTTON.DEFAULT}
-        />
-      </Tooltip>
-      <Tooltip title={t('Web/Hybrid App Mode')}>
-        <Button
-          icon={<IconWorld size={18} />}
-          onClick={() => selectAppMode(APP_MODE.WEB_HYBRID)}
-          type={appMode === APP_MODE.WEB_HYBRID ? BUTTON.PRIMARY : BUTTON.DEFAULT}
-        />
-      </Tooltip>
-      {contexts && contexts.length === 1 && (
-        <Tooltip title={t('noAdditionalContextsFound')} classNames={{root: styles.wideTooltip}}>
-          <Button
-            disabled
-            icon={<IconExclamationCircle size={20} />}
-            styles={{root: {backgroundColor: '#faad14', color: '#ffffff'}}}
-          />
-        </Tooltip>
-      )}
-      {contexts && contexts.length > 1 && (
-        <>
-          <Select
-            styles={{root: {width: 350}}}
-            value={currentContext}
-            popupMatchSelectWidth={false}
-            onChange={(value) => {
-              setContext(value);
-              applyClientMethod({methodName: 'switchAppiumContext', args: [value]});
-            }}
-            options={contexts.map(({id, title}) => ({
-              value: id,
-              label: title ? `${title} (${id})` : id,
-            }))}
-          />
-          <Tooltip
-            title={
-              <>
-                {t('contextDropdownInfo')}{' '}
-                <a onClick={(e) => e.preventDefault() || openLink(LINKS.HYBRID_MODE_DOCS)}>
-                  {LINKS.HYBRID_MODE_DOCS}
-                </a>
-              </>
-            }
-            classNames={{root: styles.wideTooltip}}
-          >
-            <Button
-              disabled
-              icon={<IconInfoCircle size={20} />}
-              styles={{root: {backgroundColor: 'var(--ant-color-primary)', color: '#ffffff'}}}
-            />
-          </Tooltip>
-        </>
-      )}
-    </Space.Compact>
-  );
-
   return (
     <div className={styles.headerButtons}>
       <Space size="middle">
         {deviceControls}
         {automationName === DRIVERS.UIAUTOMATOR2 && displayControls}
-        {appModeControls}
+        <ContextControlsGroup
+          selectAppMode={selectAppMode}
+          appMode={appMode}
+          contexts={contexts}
+          currentContext={currentContext}
+          setContext={setContext}
+          applyClientMethod={applyClientMethod}
+          openLink={openLink}
+        />
         <GeneralControlsGroup {...props} />
         <SessionReloadButton
           autoSessionRestart={autoSessionRestart}
