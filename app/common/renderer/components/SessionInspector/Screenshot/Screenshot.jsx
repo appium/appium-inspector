c5776d278c6ad9a191ae9d400382e045bf1c039e
Edgars Eglītis <edgars.eglitis@outlook.com>
2026-09-07 13:05:01 +0300

    chore: address comments

--- app/common/renderer/components/SessionInspector/Screenshot/Screenshot.jsx
+++ app/common/renderer/components/SessionInspector/Screenshot/Screenshot.jsx
@@ -29,41 +29,33 @@ const ScreenshotOuterSpinner = () => (
 );
 
 // Calculate the target screenshot panel width (unless changed manually),
-// as well as the scale ratio of the original screenshot width to its target rendered width,
+// as well as the scale ratio of the raw screenshot width to its target rendered width,
 // which is used to position screenshot overlay items like highlighters and gesture trails.
-// The image itself is rescaled purely using CSS. Scale calculation depends on the original image dimensions,
-// the width of the screenshot panel, and the width/height of the entire Inspector window.
-const updateScreenshotScale = (
-  screenshotContentElRef,
-  setScaleRatio,
-  rawImageSize,
-  screenshotPanelWidth,
-  suggestScreenshotPanelWidth,
-) => {
-  const screenshotContent = screenshotContentElRef.current;
-  if (!screenshotContent) {
+// The image itself is rescaled purely using CSS.
+const updateScreenshotScale = ({sizes, setScaleRatio, suggestScreenshotPanelWidth}) => {
+  if (!sizes.screenshotContent) {
     return;
   }
 
-  const availableWidth = screenshotContent.clientWidth;
-  const availableHeight = screenshotContent.clientHeight;
+  const availableWidth = sizes.screenshotContent.clientWidth;
+  const availableHeight = sizes.screenshotContent.clientHeight;
   if (!availableWidth || !availableHeight) {
     return;
   }
 
-  const imageWidthToFitHeight = (availableHeight * rawImageSize.width) / rawImageSize.height;
+  const imageWidthToFitHeight = (availableHeight * sizes.rawImage.width) / sizes.rawImage.height;
   const maxAllowedImageWidth = window.innerWidth * WINDOW_DIMENSIONS.MAX_IMG_WIDTH_FRACTION;
-  // check rawImageSize width too, since it may be the smallest (e.g. for smartwatches)
-  const bestFitWidth = Math.min(imageWidthToFitHeight, maxAllowedImageWidth, rawImageSize.width);
+  // check rawImage width too, since it may be the smallest (e.g. for smartwatches)
+  const bestFitWidth = Math.min(imageWidthToFitHeight, maxAllowedImageWidth, sizes.rawImage.width);
   // the Splitter's own resize bar eats into the panel's requested size, so the content area
   // ends up slightly narrower than requested - add that difference
-  const splitterOverhead = screenshotPanelWidth - availableWidth;
+  const splitterOverhead = sizes.screenshotPanelWidth - availableWidth;
   suggestScreenshotPanelWidth(bestFitWidth + splitterOverhead);
   // users are allowed to set the screenshot container width to exceed the image width
   // (the image size stops increasing once height-bound),
   // so for the scale ratio, use the container-available and height-fit widths
   const renderedWidth = Math.min(availableWidth, imageWidthToFitHeight);
-  setScaleRatio(rawImageSize.width / renderedWidth);
+  setScaleRatio(sizes.rawImage.width / renderedWidth);
 };
 
 /**
@@ -110,20 +102,22 @@ const Screenshot = (props) => {
     [isAwaitingMjpegStream, serverDetails.mjpegScreenshotUrl, setAwaitingMjpegStream],
   );
 
-  // set up triggers for recalculating of the screenshot image scaling factor:
-  // changes in size for the raw image, MJPEG stream, screenshot panel, and the Inspector window
+  // set up triggers for recalculating the screenshot image scaling factor, upon changes in size
+  // for the raw image, MJPEG stream, screenshot panel, and the Inspector window
   useEffect(() => {
     if (!windowSize || !JSON.stringify(windowSize)) {
       return;
     }
     const debouncedUpdateScale = debounce(() => {
-      updateScreenshotScale(
-        screenshotContentElRef,
+      updateScreenshotScale({
+        sizes: {
+          rawImage: windowSize,
+          screenshotContent: screenshotContentElRef.current,
+          screenshotPanelWidth,
+        },
         setScaleRatio,
-        windowSize,
-        screenshotPanelWidth,
         suggestScreenshotPanelWidth,
-      );
+      });
     }, 50);
     debouncedUpdateScale();
     window.addEventListener('resize', debouncedUpdateScale);
