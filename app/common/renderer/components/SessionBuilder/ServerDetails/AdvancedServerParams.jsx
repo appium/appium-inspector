140ba6863e7c167df4118fc37e15edc5a0c9f7fb
Edgars Eglītis <edgars.eglitis@outlook.com>
2026-09-17 18:22:51 +0300

    style: apply RWD to advanced settings

--- app/common/renderer/components/SessionBuilder/ServerDetails/AdvancedServerParams.jsx
+++ app/common/renderer/components/SessionBuilder/ServerDetails/AdvancedServerParams.jsx
@@ -1,4 +1,4 @@
-import {Checkbox, Col, Collapse, Form, Input, Row} from 'antd';
+import {Checkbox, Collapse, Input} from 'antd';
 import {useTranslation} from 'react-i18next';
 
 import {SERVER_ADVANCED_PARAMS, SERVER_TYPES} from '../../../constants/session-builder.js';
@@ -7,67 +7,52 @@ import styles from './ServerDetails.module.css';
 
 const AdvancedServerParams = ({server, setServerParam, serverType}) => {
   const {t} = useTranslation();
+
   return (
-    <Row gutter={8}>
-      <Col className={styles.advancedSettingsContainerCol}>
-        <div className={styles.advancedSettingsContainer}>
-          <Collapse
-            styles={{header: {padding: '8px'}, body: {padding: '8px'}}}
-            items={[
-              {
-                label: t('Advanced Settings'),
-                children: (
-                  <Row>
-                    {serverType !== SERVER_TYPES.TESTMUAI && (
-                      <Col span={7}>
-                        <Form.Item>
-                          <Checkbox
-                            checked={!!server.advanced.allowUnauthorized}
-                            onChange={(e) =>
-                              setServerParam(
-                                SERVER_ADVANCED_PARAMS.ALLOW_UNAUTHORIZED,
-                                e.target.checked,
-                                SERVER_TYPES.ADVANCED,
-                              )
-                            }
-                          >
-                            {t('allowUnauthorizedCerts')}
-                          </Checkbox>
-                        </Form.Item>
-                      </Col>
-                    )}
-                    <Col span={5} align="right">
-                      <Form.Item>
-                        <Checkbox
-                          checked={!!server.advanced.useProxy}
-                          onChange={(e) =>
-                            setServerParam(SERVER_ADVANCED_PARAMS.USE_PROXY, e.target.checked, SERVER_TYPES.ADVANCED)
-                          }
-                        >
-                          {t('Use Proxy')}
-                        </Checkbox>
-                      </Form.Item>
-                    </Col>
-                    <Col span={8}>
-                      <Form.Item>
-                        <Input
-                          disabled={!server.advanced.useProxy}
-                          onChange={(e) =>
-                            setServerParam(SERVER_ADVANCED_PARAMS.PROXY, e.target.value, SERVER_TYPES.ADVANCED)
-                          }
-                          placeholder={t('Proxy URL')}
-                          value={server.advanced.proxy}
-                        />
-                      </Form.Item>
-                    </Col>
-                  </Row>
-                ),
-              },
-            ]}
-          />
-        </div>
-      </Col>
-    </Row>
+    <Collapse
+      styles={{header: {padding: '8px'}, body: {padding: '8px'}}}
+      items={[
+        {
+          label: t('Advanced Settings'),
+          children: (
+            <div className={styles.advancedSettingsRow}>
+              {serverType !== SERVER_TYPES.TESTMUAI && (
+                <div className={styles.serverInputField250px}>
+                  <Checkbox
+                    className={styles.addonCheckbox}
+                    checked={!!server.advanced.allowUnauthorized}
+                    onChange={(e) =>
+                      setServerParam(SERVER_ADVANCED_PARAMS.ALLOW_UNAUTHORIZED, e.target.checked, SERVER_TYPES.ADVANCED)
+                    }
+                  >
+                    {t('allowUnauthorizedCerts')}
+                  </Checkbox>
+                </div>
+              )}
+              <div className={styles.serverInputGroup500px}>
+                <Checkbox
+                  styles={{root: {minWidth: '100px'}}}
+                  className={styles.addonCheckbox}
+                  checked={!!server.advanced.useProxy}
+                  onChange={(e) =>
+                    setServerParam(SERVER_ADVANCED_PARAMS.USE_PROXY, e.target.checked, SERVER_TYPES.ADVANCED)
+                  }
+                >
+                  {t('Use Proxy')}
+                </Checkbox>
+                <Input
+                  styles={{root: {maxWidth: '400px'}}}
+                  disabled={!server.advanced.useProxy}
+                  onChange={(e) => setServerParam(SERVER_ADVANCED_PARAMS.PROXY, e.target.value, SERVER_TYPES.ADVANCED)}
+                  placeholder={t('Proxy URL')}
+                  value={server.advanced.proxy}
+                />
+              </div>
+            </div>
+          ),
+        },
+      ]}
+    />
   );
 };
 
